705e119ada6dc9c517c7fe6a801bdccf64ca27ac
Chris Murphy <chmurphy@redhat.com>
2017-02-07 12:50:33 -0500

    Move create_cicd_cluster.sh changes into repo, sanitize scripts by sourcing private files, add .gitignore.

--- bin/create_cicd_cluster.sh
+++ bin/create_cicd_cluster.sh
@@ -1,23 +1,47 @@
-#!/bin/bash -e
+#!/bin/bash
+#
+set -euo pipefail
 
 # Kill all background jobs on normal exit or signal
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-export AWS_CREDENTIALS_FILE=/home/opsmedic/.aws/credentials
-export IDP_HOST=login.ops.openshift.com
-export AWS_ACCOUNT_ID=639866565627
-export AWS_ACCOUNT_NAME=opstest
+# Install ssh key to allow us to ssh locally
+pushd ~/aos-cd/git/aos-cd-jobs/vars
+  source private/setup_opstest_vars_private.sh
+  echo "Source in Cloud Specific Vars"
+popd
 
 # Install ssh key to allow us to ssh locally
-./add_ssh_key_to_localhost.yml
+pushd ~/aos-cd/git/aos-cd-jobs/bin
+  ./add_ssh_key_to_localhost.yml
+  echo "Add loopback ssh"
+popd
 
-cp cicd_aws_cluster_setup.yml ../git/openshift-ansible-ops/playbooks/release/bin
+# Update cluster setup changes to the releases directory
+pushd ~/aos-cd/git/aos-cd-jobs/private/files
+  cp cicd_aws_cluster_setup.yml ../../../openshift-ansible-ops/playbooks/release/bin
+  echo "Update cluster setup changes"
+popd
 
-pushd ../git/openshift-ansible-ops/playbooks/release/bin
+# Deploy all the things
+pushd ~/aos-cd/git/openshift-ansible-ops/playbooks/release/bin
   autokeys_loader ./refresh_aws_tmp_credentials.py --refresh &
   export AWS_DEFAULT_PROFILE=$AWS_ACCOUNT_NAME
   autokeys_loader ./aws_cluster_setup.sh cicd
+  # Assume we succeed for now.
+  return 0
 popd
 
 # Remove ssh key to allow us to ssh locally
-./add_ssh_key_to_localhost.yml -e ssh_key_state=absent
+pushd ~/aos-cd/git/aos-cd-jobs/bin
+  ./add_ssh_key_to_localhost.yml -e ssh_key_state=absent
+  echo "Remove loopback ssh"
+popd
+
+echo
+echo "Deployment is complete. OpenShift Console can be found at https://${MASTER_DNS_NAME}"
+echo
+
+exit 0
+
+# vim:sw=2:ts=2:softtabstop=2:expandtab:textwidth=79

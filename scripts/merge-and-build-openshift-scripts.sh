fc980b9b95d79093c844af4ce807214b1ac840ad
Samuel Munilla <smunilla@redhat.com>
2017-03-02 13:23:29 -0500

    Initial commit of openshift-online build automation
    New script in a new branch to all for daily builds of
    openshift-scripts rpm.

---
+++ scripts/merge-and-build-openshift-scripts.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+set -o xtrace
+
+kinit -k -t $KEYTAB $PRINCIPLE
+
+MB_PATH=$(readlink -f $0)
+SCRIPTS_DIR=$(dirname $MB_PATH)
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Use the directory relative to this Jenkins job.
+BUILDPATH="${WORKSPACE}/go"
+mkdir -p $BUILDPATH
+cd $BUILDPATH
+export GOPATH="$( pwd )"
+WORKPATH="${BUILDPATH}/src/github.com/openshift/"
+mkdir -p $WORKPATH
+echo "GOPATH: ${GOPATH}"
+echo "BUILDPATH: ${BUILDPATH}"
+echo "WORKPATH ${WORKPATH}"
+
+# Ensure ssh-agent is running
+eval "$(ssh-agent -s)"
+
+# Load deploy key for cloning/pushing openshift/openshift-online
+ssh-add -D
+ssh-add ${HOME}/.ssh/openshift-online/id_rsa
+
+rm -rf online
+git clone git@github.com:openshift/online.git
+cd online/
+
+# Check to see if there have been any changes since the last tag
+if git describe --abbrev=0 --tags --exact-match HEAD >/dev/null 2>&1; then
+    echo ; echo "No changes since last tagged build"
+    echo "This is fine, continuing build"
+else
+    #There have been changes, so rebuild
+    echo
+    echo "=========="
+    echo "Tito Tagging"
+    echo "=========="
+    tito tag --accept-auto-changelog
+    git push
+    git push --tags
+
+    echo
+    echo "=========="
+    echo "Tito building in brew"
+    echo "=========="
+    TASK_NUMBER=`tito release --yes --test brew | grep 'Created task:' | awk '{print $3}'`
+    echo "TASK NUMBER: ${TASK_NUMBER}"
+    echo "TASK URL: https://brewweb.engineering.redhat.com/brew/taskinfo?taskID=${TASK_NUMBER}"
+    echo
+    brew watch-task ${TASK_NUMBER}
+
+    echo
+    echo "=========="
+    echo "Tagging package in brew"
+    echo "=========="
+    TAG=`git describe --abbrev=0`
+    COMMIT=`git log -n 1 --pretty=%h`
+    brew tag-pkg libra-rhel-7-candidate ${TAG}.git.0.${COMMIT}.el7
+fi
+
+echo
+echo
+echo "=========="
+echo "Finished"
+echo "OCP ${VERSION}"
+echo "=========="

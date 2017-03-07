7ee0a22b4b4fed889b88784914cc1656c7e13c27
Jim Minter <jminter@redhat.com>
2017-03-07 11:09:23 +0000

    Don't rebuild integration and extended test binaries; use the ones already compiled within the release container

--- commands/test_pull_requests_origin_integration_future.sh
+++ commands/test_pull_requests_origin_integration_future.sh
@@ -3,4 +3,4 @@ go get github.com/openshift/imagebuilder/cmd/imagebuilder
 export OS_BUILD_IMAGE_ARGS=''
 hack/build-base-images.sh
 make release
-JUNIT_REPORT='true' make test -o check -k
+OPENSHIFT_SKIP_BUILD=1 JUNIT_REPORT='true' make test -o check -k

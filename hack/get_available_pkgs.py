40e39336e0c177de36b722672cc57f0624fca31f
jhadvig <jhadvig@redhat.com>
2017-03-14 13:40:26 -0400

    Set disableexcludes flag when fetching versions

--- hack/get_available_pkgs.py
+++ hack/get_available_pkgs.py
@@ -2,6 +2,7 @@ import yum
 import sys
 
 yb = yum.YumBase()
+yb.conf.disable_excludes = ["all"]
 pkgs = yb.doPackageLists('available', patterns=[sys.argv[1]], showdups=True).available
 pkgs.sort(key=lambda x: x.version)
 for p in pkgs:

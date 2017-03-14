a40f4cd678616b0b8c353adfb563c104a6ac0dca
Bruno Barcarol Guimarães <bbarcaro@redhat.com>
2017-03-14 08:44:13 +0000

    Move publish-oc-binary.sh to build-scripts.

--- scripts/merge-and-build.sh
+++ scripts/merge-and-build.sh
@@ -249,32 +249,8 @@ echo "=========="
 echo "Publish the oc binary"
 echo "=========="
 ssh ocp-build@rcm-guest.app.eng.bos.redhat.com \
-    sh -s "$OSE_VERSION" "${VERSION#v}" <<-'EOF'
-	set -eux
-	OSE_VERSION=$1
-	VERSION=$2
-	rpm=/mnt/rcm-guest/puddles/RHAOS/AtomicOpenShift/$OSE_VERSION/latest/x86_64
-	rpm=$rpm/os/Packages/atomic-openshift-clients-redistributable-$VERSION
-	rpm=$(echo "$rpm"*)
-	tmpdir=$(mktemp -dt ocbinary.XXXXXXXXXX)
-	trap "rm -rf '$tmpdir'" EXIT INT TERM
-	mkdir -p "$tmpdir/$VERSION/"{linux,macosx,windows}
-	cd "$tmpdir"
-	rpm2cpio "$rpm" | cpio -idm --quiet
-	cd "$tmpdir/usr/share/atomic-openshift"
-	outdir=$tmpdir/$VERSION
-	tar --owner 0 --group 0 -C linux/ -zc oc -f "$outdir/linux/oc.tar.gz"
-	tar --owner 0 --group 0 -C macosx/ -zc oc -f "$outdir/macosx/oc.tar.gz"
-	zip --quiet --junk-path - windows/oc.exe > "$outdir/windows/oc.zip"
-	rsync \
-		-av --delete-after --progress --no-g --omit-dir-times --chmod=Dug=rwX \
-		-e "ssh -l jenkins_aos_cd_bot -o StrictHostKeyChecking=no" \
-		"$outdir" \
-		use-mirror-upload.ops.rhcloud.com:/srv/pub/openshift-v3/clients/
-	ssh -l jenkins_aos_cd_bot -o StrictHostKeychecking=no \
-		use-mirror-upload.ops.rhcloud.com \
-		/usr/local/bin/push.pub.sh openshift-v3 -v
-EOF
+    sh -s "$OSE_VERSION" "${VERSION#v}" \
+    < "$WORKSPACE/build-scripts/rcm-guest/publish-oc-binary.sh"
 for x in "${VERSION#v}/"{linux/oc.tar.gz,macosx/oc.tar.gz,windows/oc.zip}; do
     curl --silent --show-error --head \
         "https://mirror.openshift.com/pub/openshift-v3/clients/$x" \

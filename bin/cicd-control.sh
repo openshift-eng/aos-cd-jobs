f39b6be0d9c9dedac1418a746dc079099cf3725c
Matt Woodson <mwoodson@redhat.com>
2017-04-06 16:55:25 -0400

    put in a check around the delete operation

--- bin/cicd-control.sh
+++ bin/cicd-control.sh
@@ -52,15 +52,15 @@ export VERSION=3.5
 # For now, all we will do is echo out the $CLUSTERNAME and $OPERATION variables
 # and then exit successfully.
 if  [ "${CLUSTERNAME}" == "test-key" ]; then
-  echo "Operation requested on mock cluster '${CLUSTERNAME}'. The operation is: '${OPERATION}' with options: ${ARGS}" 
+  echo "Operation requested on mock cluster '${CLUSTERNAME}'. The operation is: '${OPERATION}' with options: ${ARGS}"
   echo "  VERSION=${VERSION}"
   echo "  OPENSHIFT_ANSIBLE_VERSION=${OPENSHIFT_ANSIBLE_VERSION}"
   exit 0
 fi
 
 # update git repos
-# This needs review. 
-# This isn't very portable. This requires that the git dirs are already 
+# This needs review.
+# This isn't very portable. This requires that the git dirs are already
 # in place to do updates
 ####### DEBUG. UNCOMMENT THE FOLLOWING FOR PRODUCTION ####
 /usr/bin/ansible-playbook ./clone_ops_git_repos.yml
@@ -74,49 +74,54 @@ if [ ! -f ${CLUSTER_SETUP_TEMPLATE_FILE} ]; then
   echo "Unable to find ${CLUSTERNAME}'s cluster setup template file. Exiting..."
   exit 10
 fi
-  
+
 # Update cluster setup changes to the releases directory
 echo "Update cluster setup changes..."
 /usr/bin/cp ${CLUSTER_SETUP_TEMPLATE_FILE} ../../openshift-ansible-ops/playbooks/release/bin
- 
+
 
 
 
 ################################################
 # CREATE CLUSTER
 ################################################
-if [ "${OPERATION}" == "install" ]; then 
-  
+if [ "${OPERATION}" == "install" ]; then
+
   # Deploy all the things
   pushd ~/aos-cd/git/openshift-ansible-ops/playbooks/release/bin
     /usr/local/bin/autokeys_loader ./refresh_aws_tmp_credentials.py --refresh &> /dev/null &
-    
+
     # Kill all background jobs on normal exit or signal
-    trap 'kill $(jobs -p)' EXIT 
+    trap 'kill $(jobs -p)' EXIT
 
     export AWS_DEFAULT_PROFILE=$AWS_ACCOUNT_NAME
     export SKIP_GIT_VALIDATION=TRUE
     /usr/local/bin/autokeys_loader ./aws_cluster_setup.sh ${CLUSTERNAME}
   popd
-  
+
   echo
   echo "Deployment is complete. OpenShift Console can be found at https://${MASTER_DNS_NAME}"
   echo
-  
+
 ################################################
 # DELETE CLUSTER
 ################################################
-elif [ "${OPERATION}" == "delete" ]; then 
+elif [ "${OPERATION}" == "delete" ]; then
+
+  # another layer of protection for delete clusters
+  if [[ "${CLUSTERNAME}" != "dev-preview-int" ]] && [[ "${CLUSTERNAME}" != "cicd" ]]; then
+    echo "INVALID CLUSTER, NOT DELTETING CLUSTER: '${CLUSTERNAME}'. Exiting...."
+    exit 10
+  fi
 
   # This updates the OPs inventory
   echo "Updating the OPs inventory..."
   /usr/share/ansible/inventory/multi_inventory.py --refresh-cache --cluster=${CLUSTERNAME} >/dev/null
   echo
-  
-  pushd ../../openshift-ansible-ops/playbooks/release/decommission
-    /usr/local/bin/autokeys_loader /usr/bin/ansible-playbook aws_remove_cluster.yml -e cli_clusterid=${CLUSTERNAME} -e cluster_to_delete=${CLUSTERNAME} -e run_in_automated_mode=True
+
+  pushd ../../openshift-ansible-ops/playbooks/release/decommission /usr/local/bin/autokeys_loader /usr/bin/ansible-playbook aws_remove_cluster.yml -e cli_clusterid=${CLUSTERNAME} -e cluster_to_delete=${CLUSTERNAME} -e run_in_automated_mode=True
   popd
-  
+
   # This updates the OPs inventory
   echo "Updating the OPs inventory..."
   /usr/share/ansible/inventory/multi_inventory.py --refresh-cache --cluster=${CLUSTERNAME} >/dev/null
@@ -124,21 +129,21 @@ elif [ "${OPERATION}" == "delete" ]; then
 ################################################
 # UPGRADE CLUSTER
 ################################################
-elif [ "${OPERATION}" == "upgrade" ]; then 
+elif [ "${OPERATION}" == "upgrade" ]; then
   echo Doing upgrade
 
-  # Run the upgrade, including post_byo steps and config loop 
+  # Run the upgrade, including post_byo steps and config loop
   pushd ~/aos-cd/git/openshift-ansible-ops/playbooks/release/bin
     /usr/local/bin/autokeys_loader ./refresh_aws_tmp_credentials.py --refresh &> /dev/null &
-    
+
     # Kill all background jobs on normal exit or signal
-    trap 'kill $(jobs -p)' EXIT 
+    trap 'kill $(jobs -p)' EXIT
 
     export AWS_DEFAULT_PROFILE=$AWS_ACCOUNT_NAME
     export SKIP_GIT_VALIDATION=TRUE
     /usr/local/bin/autokeys_loader ./aws_online_cluster_upgrade.sh ./ops-to-productization-inventory.py ${CLUSTERNAME}
   popd
- 
+
 else
   echo Error. Unrecognized operation. Exiting...
 fi

3f186810227ea74f34697b1ed48a3fd40913c0d1
Jakub Hadvig <jhadvig@redhat.com>
2017-06-12 11:32:14 +0200

    Continuous upgrade job for CICD cluster

--- continuous-upgrade/generate-jobs.sh
+++ continuous-upgrade/generate-jobs.sh
@@ -4,9 +4,9 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-jobs=( provision upgrade terminate )
+jobs=( provision upgrade terminate cicd-upgrade )
 
-pushd sjb
+pushd continuous-upgrade
 for job in "${jobs[@]}"; do
     jenkins-jobs test jobs/${job}-job.yml > generated/continuous-upgrade_${job}-job.xml
 done

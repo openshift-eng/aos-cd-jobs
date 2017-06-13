235144d3afaa36436e3251bdd7920988178b4530
Justin Pierce <jupierce@redhat.com>
2017-06-13 11:09:18 -0400

    Adding image pushes & ose_images cleanup

--- jobs/build/scheduled/buildvm-maintenance/scripts/maintenance.sh
+++ jobs/build/scheduled/buildvm-maintenance/scripts/maintenance.sh
@@ -3,6 +3,10 @@
 set -o xtrace
 set -e
 
+echo "====Cleaning up old ose_images.sh temporary directories===="
+find /var/tmp/ose_images* -maxdepth 0 -mtime +4 -exec rm -r {} \;
+
+echo "====Cleaning up older docker images===="
 # Buildvm pulls images from brew/pulp constantly in order to push them to registry.ops .
 # If we don't clean out the docker images, it will fill up any drive over time.
 # If we clean them all, image pulls from pulp will be extremely slow because we won't
@@ -27,5 +31,18 @@ set -e
         done
 } | sort | awk '{ print $2 }' | uniq | head -n -100 | xargs --no-run-if-empty docker sudo rmi -f
 
+echo "====Pushing logging and metrics images===="
+ose_images.sh push_images --branch rhaos-3.4-rhel-7 --group logging --group metrics --nolatest
+ose_images.sh push_images --branch rhaos-3.5-rhel-7 --group logging --group metrics --nolatest
+ose_images.sh push_images --branch rhaos-3.6-rhel-7 --group logging --group metrics
+
+set +e
+ose_images.sh push_images --branch rhaos-3.6-rhel-8 --group logging --group metrics
+if [ "$?" == "0" ]; then
+        echo "A new branch has been introduced, script needs to be updated."
+fi
+set -e
+
+echo "====Docker statistics===="
 # Print out a report for the Jenkins job
 sudo docker info

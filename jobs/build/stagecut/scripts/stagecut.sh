f83df46172011fe1fec725e3d78ad1e00d0275b5
Justin Pierce <jupierce@redhat.com>
2017-06-27 21:26:56 -0400

    Stagecut should no longer muck with version fields in openshift-ansible
    master will always use 3 fields
    stage will always use 4
    enterprise will always use 5
    
    This way, there will be no conflicts between any stream or CI.

--- jobs/build/stagecut/scripts/stagecut.sh
+++ jobs/build/stagecut/scripts/stagecut.sh
@@ -47,34 +47,7 @@ for repo in $@; do
             git push origin stage
         fi
 
-        # If this is the openshift-ansible repository, we need to take an extra
-        # step to ensure CI continues working. During the installer CI job,
-        # master is extracted and tito tag is run. The tag created must be unique. Prior
-        # to stagecut, this is always true. During stagecut, however,
-        # openshift-ansible.spec in master falls behind the tags being created
-        # by builds in stage.
-        # To resolve this, during stagecut, openshift-ansible.spec in master
-        # is tweaked to have four fields instead of 3 so that CI tito tags
-        # won't conflict with tags created by stage builds.
-        #.1000 is used as the four field because there are potentially moments
-        # when using .0 could conflict with the numbering of a release cut of OCP.
-        if [ "$DESTRUCTIVE_SYNCH" != "true" -a -f "openshift-ansible.spec" ]; then
-            git checkout master
-            export VERSION="$(grep Version: openshift-ansible.spec | awk '{print $2}')"
-            if [ ! -z "$(echo ${VERSION} | cut -d . -f 4)" ]; then
-                echo "openshift-ansible already contains a 4 field Version in spec file. Something is wrong, so refusing to proceed."
-                echo "Performing a standard openshift-ansible build after stagecut ended should have restored a three field version."
-                exit 1
-            fi
-            VERSION="${VERSION}.1000" # Add another field
-            sed -i "s/Version:.*/Version:        ${VERSION}/" openshift-ansible.spec
-            git add openshift-ansible.spec
-            git commit -m "Adding version field for stagecut"
-            git push origin master
-        fi
-
     popd
 
     rm -rf "${d}"
-
 done

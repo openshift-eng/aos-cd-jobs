7ac6c0e8432b79518b7566d7a077cfc1841d1aab
Justin Pierce <jupierce@redhat.com>
2017-06-28 21:54:53 -0400

    Update ose_images.sh

--- build-scripts/ose_images/ose_images.sh
+++ build-scripts/ose_images/ose_images.sh
@@ -145,11 +145,11 @@ add_group_to_list() {
           add_to_list metrics-hawkular-metrics-docker
           add_to_list metrics-hawkular-openshift-agent-docker
           add_to_list metrics-heapster-docker
-          add_to_list openshift-jenkins-docker  
-          add_to_list openshift-jenkins-2-docker 
-          add_to_list jenkins-slave-base-rhel7-docker 
-          add_to_list jenkins-slave-maven-rhel7-docker 
-          add_to_list jenkins-slave-nodejs-rhel7-docker
+#          add_to_list openshift-jenkins-docker  
+#          add_to_list openshift-jenkins-2-docker 
+#          add_to_list jenkins-slave-base-rhel7-docker 
+#          add_to_list jenkins-slave-maven-rhel7-docker 
+#          add_to_list jenkins-slave-nodejs-rhel7-docker
           add_to_list registry-console-docker
         fi
         add_to_list openshift-enterprise-openvswitch-docker

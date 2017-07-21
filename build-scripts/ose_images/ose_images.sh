fa4fbcec4978ae3af62ffe60c9c83f7d71daab27
Justin Pierce <jupierce@redhat.com>
2017-07-21 15:59:41 -0400

    Fix git url when it includes branch
    @ahaile FYI

--- build-scripts/ose_images/ose_images.sh
+++ build-scripts/ose_images/ose_images.sh
@@ -335,10 +335,10 @@ setup_git_repo() {
   # repo may be entered as github.com/reponame#branch
   # so we can secify the branch manually
   # cut out the base repo URL and the branch name, if exists
-  url=$(echo "${git_repo}#" | cut -d "#" -f 1)
+  git_base_url=$(echo "${git_repo}#" | cut -d "#" -f 1)
   branch_override=$(echo "${git_repo}#" | cut -d "#" -f 2)
   pushd "${workingdir}" >/dev/null
-  git clone -q ${git_repo} 2>/dev/null
+  git clone -q ${git_base_url} 2>/dev/null
   repo_name = $(echo "${git_path}/" | cut -d "/" -f 1)
   # get the name of the repo so we can cd into that directory for branch checkout
   pushd "${repo_name}" >/dev/null

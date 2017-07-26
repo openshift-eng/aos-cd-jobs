70c3488790dde633c9b8c80b3667650d67872107
Adam Haile <ahaile@redhat.com>
2017-07-26 07:14:57 -0400

    Stream rcm scripts to server from repo Add puddle confs to repo

--- build-scripts/rcm-guest/libra-repo-to-mirrors.sh
+++ build-scripts/rcm-guest/libra-repo-to-mirrors.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-#
-# Push the latest puddle to the mirrors
-#
+set -e
+
+# Must be run from rcm-guest host
+
 ############
 # USAGE
 ############
@@ -11,7 +12,7 @@ usage() {
   echo >&2
   echo "Build libra yum repos and push them to the mirrors. ">&2
   echo >&2
-  echo "type: int | candidate or stg | stage " >&2
+  echo "type: int | stg | release " >&2
   echo "  type of repo we are pushing" >&2
   echo >&2
   popd &>/dev/null
@@ -21,22 +22,40 @@ usage() {
 ############
 # VARIABLES
 ############
+
+# TODO: We need to handle enterprise builds of openshift-scripts
+
 TYPE="${1}"
 case "${TYPE}" in
-  int | candidate )
+  online:int | candidate )
     SOURCEREPO="Libra-Candidate"
-    DESTREPO="rhel-7-libra-candidate"
+    LEGACY_DESTREPO="rhel-7-libra-candidate"
+    DESTREPO="online-openshift-scripts-int"
   ;;
-  stg | stage )
+  online:stg | stage )
     SOURCEREPO="Libra-Stage"
-    DESTREPO="rhel-7-libra-stage"
+    LEGACY_DESTREPO="rhel-7-libra-stage"
+    DESTREPO="online-openshift-scripts-stg"
+  ;;
+  release )
+    SOURCEREPO="Libra"
+    LEGACY_DESTREPO="rhel-7-libra"
+    DESTREPO="online-openshift-scripts"
+ echo "Work is required to release runs"
+	exit 1
   ;;
   * )
-    echo "ERROR: ${TYPE} is not recognized"
+    echo "ERROR - Unknown BUILD_MODE: ${TYPE}"
     usage
   ;;
 esac
+
+# PUDDLEDIR is a symlink to the most recently created puddle.
 PUDDLEDIR="/mnt/rcm-guest/puddles/RHAOS/${SOURCEREPO}/7/latest"
+# dereference the symlink to the actual directory basename: e.g. "2017-06-09.4"
+LASTDIR=$(readlink ${PUDDLEDIR})
+
+
 MYUID="$(id -u)"
 if [ "${MYUID}" == "55003" ] ; then
   BOT_USER="-l jenkins_aos_cd_bot"
@@ -50,11 +69,21 @@ if [ "$#" -lt 1 ] ; then
 fi
 
 # Build the puddle / yum repo
-puddle -b -d /mnt/rcm-guest/puddles/RHAOS/conf/${DESTREPO}.conf -n
+puddle -b -d /mnt/rcm-guest/puddles/RHAOS/conf/${LEGACY_DESTREPO}.conf -n
+
+REPO_ROOT="/srv/enterprise/${DESTREPO}"
+ssh ${BOT_USER} -o StrictHostKeychecking=no use-mirror-upload.ops.rhcloud.com "mkdir -p ${REPO_ROOT}/${LASTDIR}"
+
+# Copy the local puddle to the new, remote location. The trailing slashes indicate that directory content and not the directory itself should be transferred.
+rsync -aHv --delete-after --no-g --omit-dir-times --chmod=Dug=rwX -e "ssh ${BOT_USER} -o StrictHostKeyChecking=no" "${PUDDLEDIR}/" use-mirror-upload.ops.rhcloud.com:${REPO_ROOT}/${LASTDIR}/
+
+# Symlink new build into all directory. Replace any existing latest directory to point to the last build.
+ssh ${BOT_USER} -o StrictHostKeychecking=no use-mirror-upload.ops.rhcloud.com "ln -sfn ${REPO_ROOT}/${LASTDIR} ${REPO_ROOT}/latest"
+
+# These are legacy locations not used by the Continuous Delivery team any longer.
+rsync -aHv --delete-after --no-g --omit-dir-times --chmod=Dug=rwX -e "ssh ${BOT_USER} -o StrictHostKeyChecking=no" ${PUDDLEDIR}/x86_64/os/ use-mirror-upload.ops.rhcloud.com:/srv/libra/${LEGACY_DESTREPO}/x86_64/
+rsync -aHv --delete-after --no-g --omit-dir-times --chmod=Dug=rwX -e "ssh ${BOT_USER} -o StrictHostKeyChecking=no" ${PUDDLEDIR}/source/ use-mirror-upload.ops.rhcloud.com:/srv/libra/${LEGACY_DESTREPO}/source/SRPMS/
 
 # Push the yum repo to the mirrors
-rsync -aHv --delete-after --no-g --omit-dir-times --chmod=Dug=rwX -e "ssh ${BOT_USER} -o StrictHostKeyChecking=no" ${PUDDLEDIR}/x86_64/os/ use-mirror-upload.ops.rhcloud.com:/srv/libra/${DESTREPO}/x86_64/
-rsync -aHv --delete-after --no-g --omit-dir-times --chmod=Dug=rwX -e "ssh ${BOT_USER} -o StrictHostKeyChecking=no" ${PUDDLEDIR}/source/ use-mirror-upload.ops.rhcloud.com:/srv/libra/${DESTREPO}/source/SRPMS/
-ssh ${BOT_USER} -o StrictHostKeychecking=no use-mirror-upload.ops.rhcloud.com /usr/local/bin/push.libra.sh ${DESTREPO} -v
-
-echo "exiting"
+ssh ${BOT_USER} -o StrictHostKeychecking=no use-mirror-upload.ops.rhcloud.com /usr/local/bin/push.libra.sh ${LEGACY_DESTREPO} -v
+ssh ${BOT_USER} -o StrictHostKeychecking=no use-mirror-upload.ops.rhcloud.com /usr/local/bin/push.enterprise.sh ${DESTREPO} -v

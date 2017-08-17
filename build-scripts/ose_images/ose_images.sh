4f654bf683abf069ca9b9065e4e5adcc031f9ff5
Justin Pierce <jupierce@redhat.com>
2017-08-16 22:14:23 -0400

    Disabling logging until correct packages are tagged

--- build-scripts/ose_images/ose_images.sh
+++ build-scripts/ose_images/ose_images.sh
@@ -136,8 +136,8 @@ add_group_to_list() {
         add_to_list logging-curator-docker
         add_to_list metrics-deployer-docker
         if [ ${MAJOR_RELEASE} != "3.3" ] && [ ${MAJOR_RELEASE} != "3.4" ]  && [ ${MAJOR_RELEASE} != "3.5" ] ; then
-          add_to_list logging-auth-proxy-docker  
-          add_to_list logging-elasticsearch-docker
+          # add_to_list logging-auth-proxy-docker  
+          # add_to_list logging-elasticsearch-docker
           # add_to_list logging-fluentd-docker
           # add_to_list logging-kibana-docker
           add_to_list metrics-cassandra-docker

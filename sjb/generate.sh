7b9a49768198d8c75add88ae9ce74a103289812f
Michal Minář <miminar@redhat.com>
2017-10-16 12:09:05 +0200

    sjb/generate.sh: use python2 if available
    Signed-off-by: Michal Minář <miminar@redhat.com>

--- sjb/generate.sh
+++ sjb/generate.sh
@@ -4,12 +4,17 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+bin="python"
+if which python2 >/dev/null 2>&1; then
+    bin="python2"
+fi
+
 pushd sjb >/dev/null
 for spec in config/test_cases/*.yml; do
-	python -m generate "${spec}" "test"
+	"${bin}" -m generate "${spec}" "test"
 done
 
 for spec in config/test_suites/*.yml; do
-	python -m generate "${spec}" "suite"
+	"${bin}" -m generate "${spec}" "suite"
 done
 popd >/dev/null

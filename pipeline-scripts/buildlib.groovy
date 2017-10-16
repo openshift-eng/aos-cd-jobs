829fee8edc75e42b4cff0b42ea062c14cf68711c
Justin Pierce <jupierce@redhat.com>
2017-10-16 09:10:00 -0400

    Moving to keytab for new build machine

--- pipeline-scripts/buildlib.groovy
+++ pipeline-scripts/buildlib.groovy
@@ -24,7 +24,9 @@ def path_setup() {
 
 def kinit() {
     echo "Initializing ocp-build kerberos credentials"
-    sh "kinit -k -t /home/jenkins/ocp-build.keytab ocp-build/atomic-e2e-jenkins.rhev-ci-vms.eng.rdu2.redhat.com@REDHAT.COM"
+    // Keytab for old os1 build machine
+    // sh "kinit -k -t /home/jenkins/ocp-build.keytab ocp-build/atomic-e2e-jenkins.rhev-ci-vms.eng.rdu2.redhat.com@REDHAT.COM"
+    sh "kinit -k -t /home/jenkins/ocp-build-buildvm.openshift.eng.bos.redhat.com.keytab ocp-build/buildvm.openshift.eng.bos.redhat.com@REDHAT.COM"
 }
 
 def registry_login() {

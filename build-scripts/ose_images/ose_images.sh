1e8f5a1bd845b0e3ff01a10c1249a6e0bf740f5e
Justin Pierce <jupierce@redhat.com>
2017-12-01 17:30:44 -0500

    Update ose_images.sh

--- build-scripts/ose_images/ose_images.sh
+++ build-scripts/ose_images/ose_images.sh
@@ -18,7 +18,7 @@ PS4='${LINENO}: '
 set -o xtrace
 
 ## LOCAL VARIABLES ##
-MASTER_RELEASE="3.8"    # Update version_trim_list when this changes
+MASTER_RELEASE="3.9"    # Update version_trim_list when this changes
 MAJOR_RELEASE="${MASTER_RELEASE}"  # This is a default if --branch is not specified
 MINOR_RELEASE=$(echo ${MAJOR_RELEASE} | cut -d'.' -f2)
 
@@ -166,7 +166,7 @@ add_group_to_list() {
           add_to_list container-engine-docker
           add_to_list ose-egress-http-proxy-docker
         fi
-        if [ ${MAJOR_RELEASE} == "3.7" ] || [ ${MAJOR_RELEASE} == "3.8" ]; then
+        if [ ${MAJOR_RELEASE} == "3.7" ] || [ ${MAJOR_RELEASE} == "3.8" || [ ${MAJOR_RELEASE} == "3.9" ]; then
           add_to_list golang-github-prometheus-prometheus-docker
           add_to_list golang-github-prometheus-alertmanager-docker
           add_to_list golang-github-openshift-prometheus-alert-buffer-docker
@@ -1195,7 +1195,7 @@ start_push_image() {
         ;;
         all-v )
           if ! [ "${NOCHANNEL}" == "TRUE" ] ; then
-            version_trim_list="v3.1 v3.2 v3.3 v3.4 v3.5 v3.6 v3.7 v3.8"
+            version_trim_list="v3.1 v3.2 v3.3 v3.4 v3.5 v3.6 v3.7 v3.8 v3.9"
             for version_trim in ${version_trim_list} ; do
               echo "  TAG/PUSH: ${PUSH_REGISTRY}/${image_path}:${version_trim}" | tee -a ${workingdir}/logs/push.image.log
               docker tag ${PULL_REGISTRY}/${image_path}:${version_version}-${release_version} ${PUSH_REGISTRY}/${image_path}:${version_trim} | tee -a ${workingdir}/logs/push.image.log

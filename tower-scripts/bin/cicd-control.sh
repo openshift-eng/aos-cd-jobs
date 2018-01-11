36e624e6cb456eca3b68d5d878f3245e1548edeb
Justin Pierce <jupierce@redhat.com>
2018-01-11 13:33:24 -0500

    Improving debug of storage migration operation

--- tower-scripts/bin/cicd-control.sh
+++ tower-scripts/bin/cicd-control.sh
@@ -269,8 +269,14 @@ function storage-migration() {
   mkdir -p "/home/opsmedic/upgrade_logs"
   SM_LOG="/home/opsmedic/upgrade_logs/${CLUSTERNAME}.storage.migrate.log"
   echo "Running storage migration and logging to: ${SM_LOG} on tower2"
-  /usr/local/bin/autokeys_loader ossh -l root "${MASTER}" -c "oc adm migrate storage --include='*' --confirm --loglevel=8" > ${SM_LOG} 2>&1
-  exit $?
+  if /usr/local/bin/autokeys_loader ossh -l root "${MASTER}" -c "oc adm migrate storage --include='*' --confirm --loglevel=8" > ${SM_LOG} 2>&1 ; then
+      echo "Storage migration ran successfully"
+      exit 0
+  else
+    echo "Storage migration failed. Running tail of ${SM_LOG} for convenience, but you can see the full log on tower2."
+    tail -n 100 "${SM_LOG}"
+    exit 1
+  fi
 }
 
 ################################################

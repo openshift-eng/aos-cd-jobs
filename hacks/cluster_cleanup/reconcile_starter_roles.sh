545d43a8cca02fa7b6fd98462ec24f95b6f164ae
Justin Pierce <jupierce@redhat.com>
2018-02-28 14:37:49 -0500

    Original version was destructive to roles we didn't want to affect

--- hacks/cluster_cleanup/reconcile_starter_roles.sh
+++ hacks/cluster_cleanup/reconcile_starter_roles.sh
@@ -5,38 +5,48 @@
 
 set -o xtrace
 
-F="/tmp/reconcile_roles.tmp"
+# Create a file that has all desired bootstrap policies
+F0="/tmp/reconcile_roles.tmp"
+oc adm create-bootstrap-policy-file --filename=$F0
 
-# Create a file that contains what all roles should be
-oc adm create-bootstrap-policy-file --filename=$F
+# Filter the template to include only roles that feed into admin/edit/view that we care about.
+# admin/edit/view were used on 3.7; the other roles are for when the cluster is using aggregation (e.g. 3.9).
+F="/tmp/reconcile_roles.cleaned"
+cat $F0 | jq '.objects = (.objects | 
+                map(    select(
+                                .metadata.name=="system:aggregate-to-admin" or
+                                .metadata.name=="system:aggregate-to-edit" or
+                                .metadata.name=="system:aggregate-to-view" or
+                                .metadata.name=="system:openshift:aggregate-to-admin" or
+                                .metadata.name=="system:openshift:aggregate-to-edit" or
+                                .metadata.name=="system:openshift:aggregate-to-view" or
+                                .metadata.name=="admin" or
+                                .metadata.name=="edit" or
+                                .metadata.name=="view" 
+                        )
+                )
+        )' > $F
 
-# Backup the current roles before updating them
+
 BACKUP=$HOME/moslasthope
 D=$(date +%s)
 mkdir -p $BACKUP
-for r in clusterrole clusterrolebinding role rolebinding ; do 
-	oc get $r -o=yaml > $BACKUP/$r.$D.org.yaml
+for r in clusterrole clusterrolebinding role rolebinding ; do
+        oc get $r -o=yaml > $BACKUP/$r.$D.org.yaml
 done
 
-# Replace the disallowed resources with junk names so that they will no longer be honored
+# Rename the dangerous resources to meaningless names in order to remove all permissions to create them.
 sed -i 's;cronjobs;cicd-is-disabling-cronjobs;g' $F
 sed -i 's;scheduledjobs;cicd-is-disabling-scheduledjobs;g' $F
 sed -i 's;routes/custom-host;cicd-is-disabling-routes/custom-host;g' $F
 
-# Some of the roles that are included in the template don't exist on the cluster so
-# replace fails to update them. However, the ones we care about are updated.
-oc process -f $F | oc replace -n openshift -f - || true
+oc process -f $F | oc replace -n openshift -f -
 
-# Capture copies of the new roles
-for r in clusterrole clusterrolebinding role rolebinding ; do 
-	oc get $r -o=yaml > $BACKUP/$r.$D.new.yaml
+for r in clusterrole clusterrolebinding role rolebinding ; do
+        oc get $r -o=yaml > $BACKUP/$r.$D.new.yaml
 done
 
-# Disable annotation of the roles. Otherwise, the next master restart will 
-# reconcile them and lose our changes.
+# annotate the resoruces to ensure the next master restart does not reconcile away our changes
 for r in system:aggregate-to-admin system:aggregate-to-edit system:aggregate-to-view system:openshift:aggregate-to-admin system:openshift:aggregate-to-edit system:openshift:aggregate-to-view admin edit view ; do 
-	oc annotate clusterrole $r openshift.io/reconcile-protect=true --overwrite || true
+        oc annotate clusterrole $r openshift.io/reconcile-protect=true --overwrite || true
 done
-
-# If anything was messed up by replace, restart a master and allow reconcile to correct it.
-systemctl restart atomic-openshift-master-api

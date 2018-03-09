4e9ccf1a8cb957722183ec236dde4b9aebd3a7c6
Vadim Rutkovsky <vrutkovs@redhat.com>
2018-03-09 20:11:41 +0100

    Use WORKSPACE instead of current dir

--- sjb/actions/download_artifacts.py
+++ sjb/actions/download_artifacts.py
@@ -11,9 +11,9 @@ ARTIFACT_DIR="$( pwd )/artifacts/gathered"
 rm -rf "${ARTIFACT_DIR}"
 mkdir -p "${ARTIFACT_DIR}"
 {%- for artifact in artifacts %}
-if ssh -F ./.config/origin-ci-tool/inventory/.ssh_config openshiftdevel sudo stat {{ artifact }}; then
-    ssh -F ./.config/origin-ci-tool/inventory/.ssh_config openshiftdevel sudo chmod -R o+rX {{ artifact }}
-    scp -r -F ./.config/origin-ci-tool/inventory/.ssh_config openshiftdevel:{{ artifact }} "${ARTIFACT_DIR}"
+if ssh -F ${WORKSPACE}/.config/origin-ci-tool/inventory/.ssh_config openshiftdevel sudo stat {{ artifact }}; then
+    ssh -F ${WORKSPACE}/.config/origin-ci-tool/inventory/.ssh_config openshiftdevel sudo chmod -R o+rX {{ artifact }}
+    scp -r -F ${WORKSPACE}/.config/origin-ci-tool/inventory/.ssh_config openshiftdevel:{{ artifact }} "${ARTIFACT_DIR}"
 fi
 {%- endfor %}
 tree "${ARTIFACT_DIR}" """)

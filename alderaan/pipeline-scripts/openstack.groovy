bd817851dec75863135905aa8f7c281d908c5f9e
Matt Bruzek <mbruzek@gmail.com>
2018-06-27 12:48:58 -0500

    Changing openstack and openshift jobs to convert properties to parameters.

--- alderaan/pipeline-scripts/openstack.groovy
+++ alderaan/pipeline-scripts/openstack.groovy
@@ -4,11 +4,11 @@ def contact = "nelluri@redhat.com"
 def pipeline_id = env.BUILD_ID
 println("Current pipeline job id is '${pipeline_id}'")
 def node_label = "'CCI && ansible-2.4'"
-def run_job = OPENSHIFT_INSTALL.toString().toUpperCase()
-def job_name = "scale-ci_install_OpenShift"
-def stage_name = "openshift_install"
-def property_file_name = "openshift.properties"
-def property_file_uri = OPENSHIFT_PROPERTY_FILE.toString()
+def run_job = OPENSTACK_INSTALL.toString().toUpperCase()
+def job_name = "scale-ci_install_OpenStack"
+def stage_name = "openstack_install"
+def property_file_name = "openstack.properties"
+def property_file_uri = OPENSTACK_PROPERTY_FILE.toString()
 
 stage (stage_name) {
 	if (run_job) {

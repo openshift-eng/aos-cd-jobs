8b55c3e95e919a400fc4961a11703ced46faaf8c
Luke Meyer <lmeyer@redhat.com>
2019-03-12 22:51:08 -0400

    ocp4: handle atomic-openshift => openshift renaming

--- build-scripts/rcm-guest/show-rpm-simple-errata-diff.sh
+++ build-scripts/rcm-guest/show-rpm-simple-errata-diff.sh
@@ -13,7 +13,7 @@ ERRATALINK="building"
 BASEDIR="/mnt/rcm-guest/puddles/RHAOS"
 SIMPLEDIR="${BASEDIR}/AtomicOpenShift/${VERSION}/${SIMPLELINK}/x86_64/os/Packages"
 ERRATADIR="${BASEDIR}/AtomicOpenShift-errata/${VERSION}/${ERRATALINK}/RH7-RHAOS-${VERSION}/x86_64/os/Packages"
-GITEXCLUDE="-e cockpit -e python-jsonschema -e python-ruamel-yaml -e python-wheel -e openshift-ansible -e atomic-openshift-clients-redistributable"
+GITEXCLUDE="-e cockpit -e python-jsonschema -e python-ruamel-yaml -e python-wheel -e openshift-ansible -e atomic-openshift-clients-redistributable -e openshift-clients-redistributable"
 
 # Find out differences, put them in a file
 echo

2d6ce4a3d1a08290184c73bea383c0097708b2e8
Adam Haile <adammhaile@gmail.com>
2019-05-13 09:38:47 -0400

    Fix set-latest.sh

--- jobs/build/set_client_latest/set-latest.sh
+++ jobs/build/set_client_latest/set-latest.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-set -eux
+set -ux
+
+SSH_OPTS="-l jenkins_aos_cd_bot -o StrictHostKeychecking=no use-mirror-upload.ops.rhcloud.com"
 
 WORKSPACE=$1
 RELEASE=$2
 
-if [[ curl -I https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${RELEASE} | grep -q "404 Not Found" ]]; then
+curl -I https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${RELEASE} | grep -q "404 Not Found"
+
+if [[ $? -eq 0 ]]; then
     echo "Release ${RELEASE} not found on mirror.openshift.com!"
     exit 1
 fi

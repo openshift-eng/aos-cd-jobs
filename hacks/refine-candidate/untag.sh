e1da651ae4f3b65111fa4863684495f6e2909a1a
Luke Meyer <lmeyer@redhat.com>
2019-09-29 07:28:38 -0400

    hacks/refine-candidate tweaks

---
+++ hacks/refine-candidate/untag.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -euo pipefail
+
+TAG="{$TAG:-${1:-}}"
+
+if [[ -z "$TAG" ]]; then
+    echo "Run only after running findit.sh and inspecting the unused-components file."
+    echo "Specify the same tag to remove unused components from e.g.:"
+    echo "  $0 rhaos-4.2-rhel-7-candidate"
+    exit 1
+fi
+
+for pkg in $(cat unused-components); do
+  echo "untagging all builds of $pkg from $TAG"
+  brew untag-build "$TAG" $(brew list-tagged "$TAG" $pkg --quiet | awk '{ print $1 }')
+done

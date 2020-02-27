54f3e780f91cb43fbab81084603fc008ff15eebb
Justin Pierce <jupierce@redhat.com>
2020-02-27 13:40:48 -0500

    Verbosity & error reporting

--- aos_cd_jobs/common.py
+++ aos_cd_jobs/common.py
@@ -6,8 +6,13 @@ from git import Repo
 
 JOBS_DIRECTORY = 'jobs'
 
+
 def initialize_repo():
     repo_dir = join(getenv('WORKSPACE'), 'aos-cd-jobs')
     if exists(repo_dir):
+        print('Using current clone of aos-cd-jobs')
         return Repo(repo_dir)
+
+    print('aos-cd-jobs not detected -- cloning a copy')
     return Repo.clone_from('git@github.com:openshift/aos-cd-jobs.git', repo_dir)
+
--- aos_cd_jobs/updater.py
+++ aos_cd_jobs/updater.py
@@ -2,17 +2,35 @@
 from os import chdir, listdir, remove, rename, walk
 from os.path import isdir, join, relpath
 from shutil import rmtree
+import pprint
+from git import PushInfo
 
 from aos_cd_jobs.common import JOBS_DIRECTORY, initialize_repo
 
+
 def update_branches(repo):
-    for job in list_jobs(repo):
+    jobs = list_jobs(repo)
+    print('Found the following jobs to update:')
+    pprint.pprint(jobs)
+
+    print('Reconciling with existing branches:')
+    pprint.pprint(repo.branches)
+
+    print('Reconciling with existing refs:')
+    pprint.pprint(repo.remotes['origin'].refs)
+
+    for job in jobs:
         if job not in repo.branches:
             if job in repo.remotes['origin'].refs:
+                print('Creating head: {job} => origin/{job}'.format(job=job))
                 repo.create_head(job, 'origin/{}'.format(job))
             else:
+                print('Creating head: {job} => master'.format(job=job))
                 repo.create_head(job, 'master')
+
+        print('Created remote branch: {job}'.format(job=job))
         create_remote_branch(repo, job)
+
 
 def list_jobs(repo):
     jobs = []
@@ -22,10 +40,12 @@ def list_jobs(repo):
             jobs.append(relpath(dirpath, jobs_directory))
     return jobs
 
+
 def create_remote_branch(repo, name):
     branch = repo.branches[name]
     populate_branch(repo, branch)
     publish_branch(repo, name)
+
 
 def populate_branch(repo, branch):
     branch.checkout()
@@ -34,10 +54,14 @@ def populate_branch(repo, branch):
     create_job_file_tree(repo, branch)
     repo.git.add(all=True)
     if not branch.repo.index.diff(branch.commit):
+        print('No difference in {} at {}'.format(branch.name, branch.commit))
         return
+
+    print('Detected a difference in {} at {}'.format(branch.name, branch.commit))
     repo.index.commit(
         'Auto-generated commit from {}'.format(
             repo.heads.master.commit.hexsha[:7]))
+
 
 def clean_file_tree(directory):
     for f in listdir(directory):
@@ -48,14 +72,21 @@ def clean_file_tree(directory):
         else:
             remove(f)
 
+
 def create_job_file_tree(repo, branch):
     job_directory = join(repo.working_dir, JOBS_DIRECTORY, branch.name)
     for f in listdir(job_directory):
         rename(join(job_directory, f), join(repo.working_dir, f))
     rmtree(join(repo.working_dir, JOBS_DIRECTORY))
 
+
 def publish_branch(repo, name):
-    repo.remotes.origin.push(name)
+    print('Pushing branch {}'.format(name))
+    for push_info in repo.remotes.origin.push(name):
+        print('Result: {}'.format(hex(push_info.flags)))
+        if push_info.flags & PushInfo.ERROR:
+            raise IOError('Error updating branch {}'.format(name))
+
 
 if __name__ == '__main__':
     repo = initialize_repo()

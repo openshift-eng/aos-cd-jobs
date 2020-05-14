2d33068f56121f86f732581113f8e6bbbd5a971e
Justin Pierce <jupierce@redhat.com>
2020-05-14 19:06:15 -0400

    Plashet concept

--- pipeline-scripts/commonlib.groovy
+++ pipeline-scripts/commonlib.groovy
@@ -33,6 +33,74 @@ ocpMergeVersions = [
 
 ocpVersions = ocp4Versions + ocp3Versions
 
+/**
+ * Why is ocp4ReleaseState needed?
+ *
+ * Before auto-signing, images were either built signed or unsigned.
+ * Unsigned images were the norm and then, right before GA, we would rebuild
+ * puddles and build as signed. In the new model, we always want to build
+ * signed **if we plan on releasing the builds via an advisory**.
+ *
+ * There are a category of builds that we DON'T plan on releasing through
+ * an advisory. For a brand new release stream (or new arch) in 4.x, we want
+ * to build nightlies and make them available as pre-release builds so that
+ * even the general public can start experimenting with the new features of the release.
+ * These pre-release images contain RPMs which have traditionally been unsigned.
+ *
+ * We need them to continue to be unsigned!
+ *
+ * The reason being that we are prohibited by product security agreements
+ * from releasing publicly any signed RPM which will not be passing through
+ * the errata process / rpmdiff.
+ *
+ * Hence we need a very explicit source of truth on where our images are
+ * destined. If it will ship via errata (state=release), we want to build signed. For
+ * early access without an errata (state=pre-release).
+ *
+ * For extra complexity, different architectures in a release can be in release
+ * or pre-release. For example, s390x might still be pre-release even after
+ * x86_64 is shipping for a given 4.x. Sooooo... we need the ability to build
+ * arch specific release payloads, during the exact same ocp4 Jenkins job / doozer run,
+ * where different architectures might be signed / unsigned.
+ *
+ * Why is the following information not in doozer. It could be.
+ * 1) I think it would need some refactoring that won't be practical until
+ * the auto-signing work is validated.
+ * 2) We normally initialize a new doozer group by copying an old one. This
+ *    release state could easily be copied unintentionally.
+ * 3) pre-release data is presently stored in poll-payload. This just tries
+ *    to make it available for other jobs.
+ *
+ * Alternatively, maybe this becomes the source of truth and confusing aspects like
+ * 'archOverrides' goes away in doozer config.
+ */
+ocp4ReleaseState = [
+        "4.6": [
+                'release': [],
+                "pre-release": [ 'x86_64', 's390x', 'ppc64le' ],
+        ],
+        "4.5": [
+            'release': [],
+            "pre-release": [ 'x86_64', 's390x', 'ppc64le' ],
+        ],
+        "4.4": [
+            "release": [ 'x86_64' ],
+            "pre-release": ['s390x', 'ppc64le'],
+        ],
+        "4.3": [
+            "release": [ 'x86_64', 's390x', 'ppc64le' ],
+            "pre-release": [],
+        ],
+        "4.2": [
+            "release": [ 'x86_64', 's390x' ],
+            "pre-release": [],
+        ],
+        "4.1": [
+            "release": [ 'x86_64' ],
+            "pre-release": [],
+        ]
+]
+
 ocpMajorVersions = [
     "4": ocp4Versions,
     "3": ocp3Versions,

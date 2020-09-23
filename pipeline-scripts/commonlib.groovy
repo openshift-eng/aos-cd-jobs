edbac659e6cb7d9d5cef099c155dd734bfdf7aad
Luke Meyer <lmeyer@redhat.com>
2020-09-23 14:36:07 -0400

    Revert "commonlib: temporarily build images with signed contents"
    This reverts commit 95f1de13fdfaaada2ee93b8fe374e5fbeb7e08dd.

--- pipeline-scripts/commonlib.groovy
+++ pipeline-scripts/commonlib.groovy
@@ -71,8 +71,8 @@ ocpVersions = ocp4Versions + ocp3Versions
  */
 ocp4ReleaseState = [
         "4.6": [
-            'release': [ 'x86_64', 's390x', 'ppc64le' ],
-            "pre-release": [ 'aarch64' ],
+            'release': [],
+            "pre-release": [ 'x86_64', 's390x', 'ppc64le', 'aarch64' ],
         ],
         "4.5": [
             'release': [ 'x86_64', 's390x', 'ppc64le' ],

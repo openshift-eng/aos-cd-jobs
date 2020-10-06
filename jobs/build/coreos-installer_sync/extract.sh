e3b214affe824f7d4430883e72440ec8d97b7ac2
Joep van Delft <jdelft@redhat.com>
2020-10-06 03:42:08 +0200

    ART-2283: Publish coreos-installer to mirror (#2373)
    This commit creates a job that downloads the RPMs of a build of
    
    The end result looks like this:
    ```
    + ssh use-mirror-upload.ops.rhcloud.com tree /srv/pub/openshift-v4/clients/coreos-installer
    /srv/pub/openshift-v4/clients/coreos-installer
    ├── latest -> v0.6.0-3
    └── v0.6.0-3
        ├── coreos-installer -> coreos-installer_amd64
        ├── coreos-installer_aarch64
        ├── coreos-installer_amd64
        ├── coreos-installer_ppc64le
        └── coreos-installer_s390x
    ```
    (This is the intermediary step, as this is not yet published!)
    
    
    
    Co-authored-by: Luke Meyer <sosiouxme@gmail.com>

---
+++ jobs/build/coreos-installer_sync/extract.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+WORKDIR="$1"
+cd "$WORKDIR"
+VERSION="$2"
+ARCHES=""
+if [[ -n "${3-}" ]]; then
+  ARCHES="${3//,/ }"
+  rm -rf keep/
+  mkdir keep/
+  for arch in $ARCHES; do
+    [[ "$arch" == "amd64" ]] && arch=x86_64
+    mv *.$arch.rpm keep/
+  done
+  rm *.rpm
+  mv keep/* .
+  rmdir keep/
+fi
+
+rm -rf "$VERSION"
+mkdir "$VERSION"
+
+for rpm in *.rpm; do
+  arch="$(awk -F'[.]' '{a = $(NF-1); print a=="x86_64" ? "amd64" : a}' <<<"$rpm")"
+  rpm2cpio "${rpm}" | cpio -idm --quiet ./usr/bin/coreos-installer
+  mv usr/bin/coreos-installer "$VERSION/coreos-installer_$arch"
+done
+
+tree "$VERSION"

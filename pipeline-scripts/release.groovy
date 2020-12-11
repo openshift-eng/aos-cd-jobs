42d4b3f8fed28019e3d5b14bc4443f84a305dec4
Shiyang Wang <shiywang@redhat.com>
2020-12-11 09:44:16 +0800

    Update pipeline-scripts/release.groovy
    Co-authored-by: Luke Meyer <sosiouxme@gmail.com>

--- pipeline-scripts/release.groovy
+++ pipeline-scripts/release.groovy
@@ -772,8 +772,8 @@ def openCincinnatiPRs(releaseName, advisory, candidate_only = false,ghorg = 'ope
                         hub pull-request -b ${ghorg}:master ${labelArgs} -h ${ghorg}:${branchName} ${messageArgs} > ${prefix}.pr
                         cat ${prefix}.pr >> ${prs_file}    # Aggregate all PRs
                         if [[ "${prefix}" == "candidate" ]]; then
-                            cat candidate.pr |awk -F 'pull/' '{print \$2}' >> ${id}
-                            hub api -XPUT "repos/${ghorg}/cincinnati-graph-data/pulls/${id}/merge" "\$@"
+                            id=`cat candidate.pr |awk -F 'pull/' '{print \$2}'`
+                            hub api -XPUT "repos/${ghorg}/cincinnati-graph-data/pulls/\$id/merge" "\$@"
                         fi
                         """
 

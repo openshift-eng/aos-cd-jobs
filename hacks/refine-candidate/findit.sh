72dca41c58bbf5a51288c6c66ea40bc49c3c4937
ximhan <ximhan@redhat.com>
2021-01-12 17:32:42 +0800

    fix env import error

--- hacks/refine-candidate/findit.sh
+++ hacks/refine-candidate/findit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #set -o xtrace
 
-TAG="{$TAG:-${1:-}}"
+TAG="${TAG:-${1:-}}"
 
 if [[ -z "$TAG" ]]; then
     echo "Run only after running download-logs.sh. Specify the same tag to find RPMs being used by container builds."

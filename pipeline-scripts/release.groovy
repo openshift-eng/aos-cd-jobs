e113e636e0f8a77aafe6ec8399475568e9e2108f
thiagoalessio <talessio@redhat.com>
2021-02-10 13:12:14 +0100

    [ART-2678] Fix opm binary location for OCP 4.6 on promote job

--- pipeline-scripts/release.groovy
+++ pipeline-scripts/release.groovy
@@ -418,15 +418,10 @@ function extract_opm() {
 
     MAJOR=$(echo "$VERSION" | cut -d . -f 1)
     MINOR=$(echo "$VERSION" | cut -d . -f 2)
-    if [ "$MAJOR" -eq 4 ] && [ "$MINOR" -le 6 ]; then
-        PREFIX=/usr/bin
-    else  # for 4.7+, opm binaries are at /usr/bin/registry/
-        PREFIX=/usr/bin/registry
-    fi
 
     PATH_ARGS=()
     for binary in ${BINARIES[@]}; do
-        PATH_ARGS+=(--path "$PREFIX/$binary:$OUTDIR")
+        PATH_ARGS+=(--path "/usr/bin/registry/$binary:$OUTDIR")
     done
 
     GOTRACEBACK=all oc -v5 image extract --confirm --only-files "${PATH_ARGS[@]}" -- "$OPERATOR_REGISTRY"

81d69a39d96717e2a4ceee5ac4fecbda21828b72
Justin Pierce <jupierce@redhat.com>
2021-05-13 15:01:57 -0400

    Inside quotes, backticks were being interpreted locally

--- jobs/build/ocp4/build.groovy
+++ jobs/build/ocp4/build.groovy
@@ -420,7 +420,7 @@ def stageMirrorRpms() {
     //  Now we have a plashet named after the original on the mirror. QE wants a 'latest' link there.
     commonlib.shell("ssh -t ${openshift_mirror_user}@${openshift_mirror_bastion} \"cd ${destBaseDir}; ln -sfn ${rpmMirror.plashetDirName} latest\"")
     //  Because the mirrors are always low on space, delete all but the lastest 4 plashets
-    commonlib.shell("ssh -t ${openshift_mirror_user}@${openshift_mirror_bastion} \"cd ${destBaseDir};  rm -rf `ls | grep '4.*' | sort | tac | tail -n +5`  \"")
+    commonlib.shell("ssh -t ${openshift_mirror_user}@${openshift_mirror_bastion} 'cd ${destBaseDir};  rm -rf `ls | grep "4.*" | sort | tac | tail -n +5`  '")
     //  For historical reasons, all puddles were linked to from 'all' directory as well.
     commonlib.shell("ssh ${openshift_mirror_user}@${openshift_mirror_bastion} -- ln -sfn ${destBaseDir}/${rpmMirror.plashetDirName} /srv/enterprise/all/${version.stream}/${rpmMirror.plashetDirName}")
     // Also establish latest link

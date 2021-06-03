82c7642dffabd2f324f1376d740c241c943aa3ae
Ximin han <ximhan@redhat.com>
2021-06-03 17:28:01 +0800

    fix ssh parameter issue
    Pseudo-terminal will not be allocated because stdin is not a terminal.

--- jobs/build/ocp4/build.groovy
+++ jobs/build/ocp4/build.groovy
@@ -430,13 +430,13 @@ def stageMirrorRpms() {
     // with the mirror infrastructure's replication to subordinates.
     commonlib.shell("ssh ${openshift_mirror_user}@${openshift_mirror_bastion} -- cp -a ${stagingDir} ${destBaseDir}/${rpmMirror.plashetDirName}")
     //  Now we have a plashet named after the original on the mirror. QE wants a 'latest' link there.
-    commonlib.shell("ssh -t ${openshift_mirror_user}@${openshift_mirror_bastion} \"cd ${destBaseDir}; ln -sfn ${rpmMirror.plashetDirName} latest\"")
+    commonlib.shell("ssh -tt ${openshift_mirror_user}@${openshift_mirror_bastion} \"cd ${destBaseDir}; ln -sfn ${rpmMirror.plashetDirName} latest\"")
     //  Because the mirrors are always low on space, delete all but the lastest 4 plashets
-    commonlib.shell("ssh -t ${openshift_mirror_user}@${openshift_mirror_bastion} \"cd ${destBaseDir};  rm -rf `ls | grep '4.*' | sort | tac | tail -n +5` \"")
+    commonlib.shell("ssh -tt ${openshift_mirror_user}@${openshift_mirror_bastion} \"cd ${destBaseDir};  rm -rf `ls | grep '4.*' | sort | tac | tail -n +5` \"")
     //  For historical reasons, all puddles were linked to from 'all' directory as well.
     commonlib.shell("ssh ${openshift_mirror_user}@${openshift_mirror_bastion} -- ln -sfn ${destBaseDir}/${rpmMirror.plashetDirName} /srv/enterprise/all/${version.stream}/${rpmMirror.plashetDirName}")
     // Also establish latest link
-    commonlib.shell("ssh -t ${openshift_mirror_user}@${openshift_mirror_bastion} \"cd /srv/enterprise/all/${version.stream}; ln -sfn ${rpmMirror.plashetDirName} latest\"")
+    commonlib.shell("ssh -tt ${openshift_mirror_user}@${openshift_mirror_bastion} \"cd /srv/enterprise/all/${version.stream}; ln -sfn ${rpmMirror.plashetDirName} latest\"")
     // Instruct the mirror infrastructure to push content to the subordinate hosts.
     commonlib.shell("ssh ${openshift_mirror_user}@${openshift_mirror_bastion} -- push.enterprise.sh -v  enterprise-${version.stream}")
     commonlib.shell("ssh ${openshift_mirror_user}@${openshift_mirror_bastion} -- push.enterprise.sh -v  all")

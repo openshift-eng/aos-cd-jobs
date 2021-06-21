495dfb2dfdb4971cffaa34cea3e75d2aeea730ab
Ximin han <ximhan@redhat.com>
2021-06-21 17:39:53 +0800

    fix publish to mirror error
    after switch to stream, directory structure changed, 
    building -> stream/building
    stream/building -> 3.11.x-1

--- build-scripts/rcm-guest/push-to-mirrors.sh
+++ build-scripts/rcm-guest/push-to-mirrors.sh
@@ -83,12 +83,15 @@ chmod 775 "${PUDDLEDIR}/" || true
 # dereference the building symlink to the actual directory basename: e.g. "2017-06-09.4"
 LASTDIR=$(readlink --verbose "${PUDDLEDIR}/building")
 
+# use time of last data modification of building dir as suffix
+CHDT=$(stat -c %Y ${LASTDIR})
+
 # Create a symlink on rcm-guest which includes the OCP version. This
 # helps find puddles on rcm-guest for particular builds. Note that
 # we can't simply rename the directory, because the directory contains
 # puddle.repo contains a URL referring to the puddle directory name
 # that was created by the puddle command.
-VERSIONED_DIR="v${FULL_VERSION}_${LASTDIR}"  # e.g. v3.7.0-0.173.0_2017-06-09.4
+VERSIONED_DIR="v${FULL_VERSION}_${CHDT}"  # e.g. v3.7.0-0.173.0_2017-06-09.4
 ln -sfn "${LASTDIR}" "${PUDDLEDIR}/${VERSIONED_DIR}"
 
 # Create the symlink on rcm-guest. QE appears to use 'latest' here instead of on mirrors.

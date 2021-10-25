8d7dbafff8ebe617444b7cc25bc89edf2f81542b
Justin Pierce <jupierce@redhat.com>
2021-10-25 17:51:26 -0400

    Ignore bundles which have not been built yet

--- scheduled-jobs/build/early-operator-index/Jenkinsfile.groovy
+++ scheduled-jobs/build/early-operator-index/Jenkinsfile.groovy
@@ -23,9 +23,11 @@ node {
     // advisories and pushed to staging.
     def pullspecs = buildlib.doozer("--group=openshift-${ocpVer} --working-dir=${workDir} olm-bundle:print " + '{bundle_pullspec}',
             [capture: true]).trim().split()
+    
+    echo "Doozer pullspecs:\n{pullspecs}"
 
     request = [
-        'bundles': pullspecs,
+        'bundles': pullspecs.findAll { it != "None" },  // Ignore bundle if it has not been built yet
         'from_index': "registry-proxy.engineering.redhat.com/rh-osbs/iib-pub-pending:v${ocpVer}",
         'binary_image': "registry-proxy.engineering.redhat.com/rh-osbs/openshift-ose-operator-registry:v${ocpVer}.0"
     ]

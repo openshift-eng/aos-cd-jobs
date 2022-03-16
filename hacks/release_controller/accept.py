1868c673cb3edc34d1e2869aaa60ec1b9836aae0
Siddharth Sharma <sidsharm@redhat.com>
2022-03-15 22:40:08 -0400

    Verify stuff

--- hacks/release_controller/accept.py
+++ hacks/release_controller/accept.py
@@ -3,6 +3,8 @@ import click
 import openshift as oc
 import json
 import time
+import re
+import requests
 
 WARNING = '\033[91m'
 ENDC = '\033[0m'
@@ -16,7 +18,8 @@ ENDC = '\033[0m'
 @click.option("--reject", type=bool, is_flag=True, default=False, help='Reject instead of accept the release')
 @click.option("--confirm", type=bool, is_flag=True, default=False,
               help="Must be specified to apply changes to server")
-def run(arch, release, upgrade_url, upgrade_minor_url, confirm, reject):
+@click.option("--allow-upgrade-to-change", type=bool, is_flag=True, default=False, help='Allow when new upgrade-to version is different from old upgrade-to')
+def run(arch, release, upgrade_url, upgrade_minor_url, confirm, reject, allow_upgrade_to_change):
 
     """
     Sets annotations to force OpenShift release acceptance.
@@ -45,6 +48,62 @@ def run(arch, release, upgrade_url, upgrade_minor_url, confirm, reject):
     if arch != 'amd64' and arch != 'x86_64':
         arch_suffix = f'-{arch}'
 
+    phase_rejected = 'Rejected'
+    phase_accepted = 'Accepted'
+    upgrade_state_success = 'Succeeded'
+    upgrade_state_failed = 'Failed'
+
+    # store upgrade test state in dict: url -> state
+    prowjobs = {}
+
+    prow_host = 'https://prow.ci.openshift.org'
+    gcs_host = 'https://gcsweb-ci.apps.ci.l2s4.p1.openshiftapps.com'
+    subdir = 'origin-ci-test/logs'
+    prow_pattern = rf'{prow_host}/view/gs/{subdir}/(.*)/(\d+)'
+
+    def assert_upgrade_test_state(release_name, arch, old_prowjob_url, new_prowjob_url, allow_upgrade_to_change=False):
+        def get_prowjob(prowjob_url):
+            if prowjob_url in prowjobs:
+                return prowjobs[prowjob_url]
+            z = re.match(prow_pattern, prowjob_url)
+            if not z or len(z.groups() != 2):
+                raise ValueError(f"given url [{prowjob_url}] doesn't match pattern [{pattern}]")
+            test_name, test_id = z.groups()
+            gcs_bucket_url = f'{gcs_host}/gcs/{subdir}/{test_name}/{test_id}/prowjob.json'
+            r = requests.get(gcs_bucket_url)
+            prowjob = oc.Model(r.json())
+            prowjobs[prowjob_url] = prowjob
+            return prowjob
+
+        # make sure existing test has failed
+        old_prowjob = get_prowjob(old_prowjob_url)
+        old_upgrade_to = old_prowjob.metadata.annotations["release.openshift.io/from-tag"]
+        if old_prowjob.status.state != upgrade_state_failed:
+            raise ValueError(f'existing prowjob {old_prowjob_url} test has state={old_prowjob.status.state} and not {upgrade_state_failed}')
+
+        # make sure new test has passed
+        new_prowjob = get_prowjob(new_prowjob_url)
+        if new_prowjob.status.state != upgrade_state_success:
+            raise ValueError(f'new prowjob {new_prowjob_url} test has state={new_prowjob.status.state} and not {upgrade_state_success}')
+
+        # make sure new test arch is same
+        new_arch = new_prowjob.metadata.annotations["release.openshift.io/architecture"]
+        if new_arch != arch:
+            raise ValueError(f'new prowjob {new_prowjob_url} test has arch={new_arch} and not {arch}')
+
+        # make sure new test release tag is same as the given release
+        new_release = new_prowjob.metadata.annotations["release.openshift.io/tag"]
+        if new_release != release_name:
+            raise ValueError(f'new prowjob {new_prowjob_url} test has release tag={new_release} and not {release_name}')
+
+        # make sure new test upgrade_to is same as old test upgrade_to
+        new_upgrade_to = new_prowjob.metadata.annotations["release.openshift.io/from-tag"]
+        if new_upgrade_to != old_upgrade_to:
+            if allow_upgrade_to_change:
+                print('new prowjob upgrade_to_version is different from old prowjob. --allow-upgrade-to-change is in effect so progressing..')
+            else:
+                raise ValueError(f'new prowjob {new_prowjob_url} has upgrade_to={new_upgrade_to} and not {old_upgrade_to}. To override pass --allow-upgrade-to-change')
+
     with oc.api_server(api_url='https://api.ci.l2s4.p1.openshiftapps.com:6443'), \
          oc.project(f'ocp{arch_suffix}'):
 
@@ -64,16 +123,25 @@ def run(arch, release, upgrade_url, upgrade_minor_url, confirm, reject):
             for annotations in (obj.model.image.metadata.annotations, obj.model.metadata.annotations, obj.model.tag.annotations):
                 annotations.pop('release.openshift.io/message', None)
                 annotations.pop('release.openshift.io/reason', None)
-                annotations['release.openshift.io/phase'] = 'Accepted' if not reject else 'Rejected'
+                phase = annotations['release.openshift.io/phase']
+                if phase != phase_rejected:
+                    raise ValueError(f'Release is phase={phase} and not {phase_rejected}. Aborting')
+                annotations['release.openshift.io/phase'] = phase_accepted if not reject else phase_rejected
 
                 verify_str = annotations['release.openshift.io/verify']
                 verify = oc.Model(json.loads(verify_str))
-                verify.upgrade.state = 'Succeeded' if not reject else 'Failed'
+
                 if upgrade_url:
+                    assert_upgrade_test_state(release, arch, verify.upgrade.url, upgrade_url, allow_upgrade_to_change)
+                    verify.upgrade.state = upgrade_state_success if not reject else upgrade_state_failed
                     verify.upgrade.url = upgrade_url
-                verify['upgrade-minor'].state = 'Succeeded' if not reject else 'Failed'
+
                 if upgrade_minor_url:
-                    verify['upgrade-minor'].url = upgrade_minor_url
+                    upgrade_minor = 'upgrade-minor'
+                    assert_upgrade_test_state(release, arch, verify[upgrade_minor].url, upgrade_minor_url, allow_upgrade_to_change)
+                    verify[upgrade_minor].url = upgrade_minor_url
+                    verify[upgrade_minor].state = upgrade_state_success if not reject else upgrade_state_failed
+
                 annotations['release.openshift.io/verify'] = json.dumps(verify._primitive(), indent=None)
 
             print(json.dumps(obj.model._primitive(), indent=4))

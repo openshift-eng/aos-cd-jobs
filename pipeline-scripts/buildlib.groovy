f4554a9a13ac0e7ddf9eda2ddd35fc87ce637b58
Ximin han <ximhan@redhat.com>
2022-08-17 14:19:23 +0800

    switch proxy

--- pipeline-scripts/buildlib.groovy
+++ pipeline-scripts/buildlib.groovy
@@ -43,7 +43,8 @@ def path_setup() {
 def proxy_setup() {
     // Take load balancer from https://source.redhat.com/departments/it/digitalsolutionsdelivery/it-infrastructure/uis/uis_wiki/squid_proxy
     // proxy = "http://proxy.squi-001.prod.iad2.dc.redhat.com:3128"
-    proxy = "http://proxy01.util.rdu2.redhat.com:3128"
+    // proxy = "http://proxy01.util.rdu2.redhat.com:3128"
+    proxy = "http://proxy01.intranet.prod.int.rdu2.redhat.com :3128"
 
     def no_proxy = [
         'localhost',

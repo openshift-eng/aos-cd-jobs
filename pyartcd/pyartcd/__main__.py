4e9ffbd787ebe293d407115445276ddfc60fe359
Yuxiang Zhu <yuxzhu@redhat.com>
2022-10-18 10:06:23 +0000

    gen-assembly: Move to Python and auto create PRs

--- pyartcd/pyartcd/__main__.py
+++ pyartcd/pyartcd/__main__.py
@@ -1,9 +1,9 @@
 from typing import Optional, Sequence
 
 from pyartcd.cli import cli
-from pyartcd.pipelines import (build_microshift, check_bugs, prepare_release,
-                               promote, rebuild, report_rhcos, review_cvp,
-                               sweep, tarball_sources)
+from pyartcd.pipelines import (build_microshift, check_bugs, gen_assembly,
+                               prepare_release, promote, rebuild, report_rhcos,
+                               review_cvp, sweep, tarball_sources)
 
 
 def main(args: Optional[Sequence[str]] = None):

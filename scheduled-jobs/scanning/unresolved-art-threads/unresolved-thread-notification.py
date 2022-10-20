0ef8a51ca056678c7650b6c33e8ec4404d2ca4d8
Justin Pierce <jupierce@redhat.com>
2022-10-20 15:23:14 +0200

    art-insights unresolved thread notification

---
+++ scheduled-jobs/scanning/unresolved-art-threads/unresolved-thread-notification.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+
+import json
+import os
+import time
+import datetime
+
+from slack_bolt import App
+
+SLACK_API_TOKEN = os.getenv('SLACK_API_TOKEN')
+SLACK_SIGNING_SECRET = os.getenv('SLACK_SIGNING_SECRET')
+USER_TOKEN = os.getenv('SLACK_USER_TOKEN')
+
+TEAM_ART_CHANNEL = 'team-art'
+RELEASE_ARTIST_HANDLE = 'release-artists'
+
+if __name__ == '__main__':
+    app = App(
+        token=SLACK_API_TOKEN,
+        signing_secret=SLACK_SIGNING_SECRET,
+    )
+
+    all_matches = []
+    next_cursor = '*'
+    while next_cursor:
+        # https://api.slack.com/methods/search.messages#examples
+        slack_response = app.client.search_messages(token=USER_TOKEN,
+                                                    query='has::art-attention: -has::art-attention-resolved:',
+                                                    cursor=next_cursor)
+        messages = slack_response.get('messages', {})
+        if messages:
+            matches = messages.get('matches', [])
+            all_matches.extend(matches)
+
+        # https://api.slack.com/docs/pagination
+        response_metadata = slack_response.get('response_metadata', {})
+        next_cursor = response_metadata.get('next_cursor', None)
+
+    all_matches = sorted(all_matches, key=lambda m: m.get('ts', '0.0'))
+
+    if not all_matches:
+        print('No messages matching attention emoji criteria')
+        exit(0)
+
+    print(json.dumps(all_matches))
+
+    fallback_text = "Currently unresolved ART threads:"
+
+    message_blocks = [
+        {
+            "type": "header",
+            "text": {
+                "type": "plain_text",
+                "text": fallback_text,
+                "emoji": True
+            }
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": f"Attention @{RELEASE_ARTIST_HANDLE}"
+            }
+        }
+    ]
+
+    current_epoch_time = time.time()
+    for match in all_matches:
+        team_id = match.get('team', '')
+        channel_id = match.get('channel', {}).get('id', '')
+        channel_name = 'Unknown'
+        if len(channel_id) > 0:
+            conv_info = app.client.conversations_info(channel=channel_id)
+            channel_name = conv_info.get('channel', {}).get('name_normalized', channel_name)
+        text = match.get('text', 'Link')
+        permalink = match.get('permalink', None)
+        if not permalink:
+            permalink = 'about:blank'
+            text = json.dumps(match)
+
+        fallback_text += f'\n{permalink}'
+        timestamp = int(float(match.get('ts', '0.0')))
+        str_date = datetime.datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+
+        age = current_epoch_time - timestamp
+        age_type = 'seconds'
+        if age > 120:
+            age /= 60
+            age_type = 'minutes'
+
+            if age >= 60:
+                age /= 60
+                age_type = 'hours'
+
+                if age >= 48:
+                    age /= 24
+                    age_type = 'days'
+
+        age = int(age)
+        # Block builder: https://app.slack.com/block-kit-builder/T04714LEPHA#%7B%22blocks%22:%5B%5D%7D
+        snippet = ' '.join(text.split(' ')[0:30])
+        message_blocks.append(
+            {
+                "type": "section",
+                "fields": [
+                    {
+                        "type": "mrkdwn",
+                        "text": f"*Channel:* <https://jupierce.slack.com/archives/{channel_id}|#{channel_name}>\n*Date:* {str_date}Z\n*Age:* {age} {age_type}"
+                    },
+                    {
+                        "type": "mrkdwn",
+                        f"text": f"*Message:* <{permalink}|Link>\n*Snippet:* {snippet}..."
+                    }
+                ]
+            },
+        )
+
+    app.client.chat_postMessage(channel=TEAM_ART_CHANNEL,
+                                text=f'@{RELEASE_ARTIST_HANDLE} - {fallback_text}',
+                                blocks=message_blocks,
+                                unfurl_links=False)

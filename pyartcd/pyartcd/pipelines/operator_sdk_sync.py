6ce191e3e71d23e2038f40a4cefabfc0cc4168a6
ximhan <ximhan@redhat.com>
2022-12-13 15:08:03 +0800

    update cmd

--- pyartcd/pyartcd/pipelines/operator_sdk_sync.py
+++ pyartcd/pyartcd/pipelines/operator_sdk_sync.py
@@ -1,12 +1,10 @@
-from github import Github
 from errata_tool import Erratum
 import yaml
 import re
-import os
 import koji
 import click
 import logging
-from pyartcd import constants, exectools, util
+import subprocess
 from pyartcd.cli import cli, click_coroutine, pass_runtime
 from pyartcd.runtime import Runtime
 _LOGGER = logging.getLogger(__name__)
@@ -19,6 +17,7 @@ class OperatorSDKPipeline:
         self.version = group.split("-")[1]
         self.assembly = assembly
         self.updatelatest = updatelatest
+        self.sdk = "operator-sdk"
 
         self._jira_client = runtime.new_jira_client()
         self._github_client = runtime.new_github_client()
@@ -31,7 +30,7 @@ class OperatorSDKPipeline:
         self.imagePath = 'registry-proxy.engineering.redhat.com/rh-osbs/openshift-ose-operator-sdk'
 
 
-    async def run(self):
+    def run(self):
         advisory = Erratum(errata_id = self.extra_ad_id)
         _LOGGER.info("Check advisory status ...")
         if advisory.errata_state in ["QE", "NEW_FILES"]:
@@ -54,43 +53,58 @@ class OperatorSDKPipeline:
         # get build digest list
         build = self.session.getBuild(sdk_build[0])
         archlist = ["amd64", "arm64", "ppc64le", "s390x"]
+        sdkVersion = self._get_sdkversion(build['extra']['image']['index']['pull'][0])
+        _LOGGER.info(sdkVersion)
         for arch in archlist:
-            await self._extract_binaries(arch, build)
+            self._extract_binaries(arch, sdkVersion, build['extra']['image']['index']['pull'][0])
         self._update_jira(self.parent_jira_key, 7, "operator_sdk_sync job : {}".format(os.environ.get("BUILD_URL")))
 
+    def _get_sdkversion(self, build):
+        output = subprocess.getoutput(f"oc image info --filter-by-os amd64 -o json {build} | jq .digest")
+        shasum = re.findall("sha256:\w*", output)[0]
+        cmd = f"oc image extract {self.imagePath}@{shasum} --path /usr/local/bin/{self.sdk}:. --confirm && chmod +x {self.sdk} && ./{self.sdk} version && rm {self.sdk}"
+        _LOGGER.info(cmd)
+        sdkvalue = subprocess.getoutput(cmd)
+        sdkversion = re.findall("v\d.*-ocp", sdkvalue)[0]
+        return sdkversion
 
-    async def _extract_binaries(self, arch, build):
-        shasum = await exectools.cmd_assert_async(f"oc image info --filter-by-os {arch} -o json {build} | jq .digest")
-        sdkVersion = await exectools.cmd_assert_async(
-            f"oc image info --filter-by-os {arch} -o json {build} | jq .config.container_config.Labels.version")
+    def _extract_binaries(self, arch, sdkVersion, build):
+        output = subprocess.getoutput(f"oc image info --filter-by-os {arch} -o json {build} | jq .digest")
+        shasum = re.findall("sha256:\w*", output)[0]
 
         rarch = arch
         rarch = 'x86_64' if arch == 'amd64' else rarch
         rarch = 'aarch64' if arch == 'arm64' else rarch
-        tarballFilename = f"operator-sdk-${sdkVersion}-linux-${rarch}.tar.gz"
+        tarballFilename = f"{self.sdk}-{sdkVersion}-linux-{rarch}.tar.gz"
 
-        cmd = f"rm -rf ./${rarch} && mkdir ./${rarch}" + \
-              f" && oc image extract ${self.imagePath}@${shasum} --path /usr/local/bin/operator-sdk:./${rarch}/ --confirm" + \
-              f" && chmod +x operator-sdk && tar -c --preserve-order -z -v --file ${tarballFilename} ./${rarch}/operator-sdk" + \
-              f" && ln -s ${tarballFilename} operator-sdk-linux-${rarch}.tar.gz && rm -f ./${rarch}/operator-sdk"
-        await exectools.cmd_assert_async(cmd)
+        cmd = f"rm -rf ./{rarch} && mkdir ./{rarch}" + \
+              f" && oc image extract {self.imagePath}@{shasum} --path /usr/local/bin/{self.sdk}:./{rarch}/ --confirm" + \
+              f" && chmod +x ./{rarch}/{self.sdk} && tar -c --preserve-order -z -v --file ./{rarch}/{tarballFilename} ./{rarch}/{self.sdk}" + \
+              f" && ln -s {tarballFilename} ./{rarch}/{self.sdk}-linux-{rarch}.tar.gz && rm -f ./{rarch}/{self.sdk}"
+        _LOGGER.info(cmd)
+        subprocess.run(cmd, shell=True)
         if arch == 'amd64':
-            tarballFilename = f"operator-sdk-${sdkVersion}-darwin-${rarch}.tar.gz"
-            cmd = f"oc image extract ${self.imagePath}@${shasum} --path /usr/share/operator-sdk/mac/operator-sdk:./${rarch}/ --confirm" + \
-                  f" && chmod +x operator-sdk && tar -c --preserve-order -z -v --file ${tarballFilename} ./${rarch}/operator-sdk" + \
-                  f" && ln -s ${tarballFilename} operator-sdk-darwin-${rarch}.tar.gz && rm -f ./${rarch}/operator-sdk"
-            await exectools.cmd_assert_async(cmd)
-        await self._sync_mirror(rarch)
+            tarballFilename = f"{self.sdk}-{sdkVersion}-darwin-{rarch}.tar.gz"
+            cmd = f"oc image extract {self.imagePath}@{shasum} --path /usr/share/{self.sdk}/mac/{self.sdk}:./{rarch}/ --confirm" + \
+                  f" && chmod +x ./{rarch}/{self.sdk} && tar -c --preserve-order -z -v --file ./{rarch}/{tarballFilename} ./{rarch}/{self.sdk}" + \
+                  f" && ln -s {tarballFilename} ./{rarch}/{self.sdk}-darwin-{rarch}.tar.gz && rm -f ./{rarch}/{self.sdk}"
+            _LOGGER.info(cmd)
+            subprocess.run(cmd, shell=True)
+        self._sync_mirror(rarch)
 
 
-    async def _sync_mirror(self, arch):
+    def _sync_mirror(self, arch):
         extra_args = "--exclude '*' --include '*.tar.gz'"
-        local_dir = f"./${arch}/"
-        s3_path = f"/pub/openshift-v4/${arch}/clients/operator-sdk/${self.assembly}/"
-        s3_path_latest = f"/pub/openshift-v4/${arch}/clients/operator-sdk/latest/"
-        await exectools.cmd_assert_async(f"aws s3 sync --no-progress --exact-timestamps ${extra_args} --delete ${local_dir} s3://art-srv-enterprise${s3_path}")
+        local_dir = f"./{arch}/"
+        s3_path = f"/pub/openshift-v4/{arch}/clients/operator-sdk/{self.assembly}/"
+        cmd = f"aws s3 sync --no-progress --exact-timestamps {extra_args} --delete {local_dir} s3://art-srv-enterprise{s3_path}"
+        _LOGGER.info(cmd)
+        subprocess.run(cmd, shell=True)
         if self.updatelatest == "true":
-            await exectools.cmd_assert_async(f"aws s3 sync --no-progress --exact-timestamps ${extra_args} --delete ${local_dir} s3://art-srv-enterprise${s3_path_latest}")
+            s3_path_latest = f"/pub/openshift-v4/{arch}/clients/operator-sdk/latest/"
+            cmd = f"aws s3 sync --no-progress --exact-timestamps {extra_args} --delete {local_dir} s3://art-srv-enterprise{s3_path_latest}"
+            _LOGGER.info(cmd)
+            subprocess.run(cmd, shell=True)
 
 
     def _update_jira(self, parent_jira_id, subtask_id, comment):
@@ -110,6 +124,6 @@ class OperatorSDKPipeline:
               help="Update latest symlink on mirror")
 @pass_runtime
 @click_coroutine
-async def tarball_sources(runtime: Runtime, group: str, assembly: str, updatelatest: bool):
+def tarball_sources(runtime: Runtime, group: str, assembly: str, updatelatest: bool):
     pipeline = OperatorSDKPipeline(runtime, group, assembly, updatelatest)
-    await pipeline.run()
+    pipeline.run()

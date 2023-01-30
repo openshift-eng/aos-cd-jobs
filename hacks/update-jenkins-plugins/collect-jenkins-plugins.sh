f65f2f5447b3766ee218d9ce3eb36569c2568105
ximhan <ximhan@redhat.com>
2023-01-30 12:31:50 +0800

    use jenkins version if latest dir not exist

--- hacks/update-jenkins-plugins/collect-jenkins-plugins.sh
+++ hacks/update-jenkins-plugins/collect-jenkins-plugins.sh
@@ -197,7 +197,7 @@ for plugin_entry in ${DEP_LIST}; do
     plugin_line=${plugin_line%\;*}
 
     stripped_entry=$(echo "${plugin_entry}:" | cut -d : -f 1)  # Strip off the version since Jenkins doesn't really honor dependency versions
-    get_plugin "${stripped_entry}" || exit 1
+    get_plugin "${stripped_entry}" || get_plugin "${plugin_entry}" || exit 1
 done
 
 echo

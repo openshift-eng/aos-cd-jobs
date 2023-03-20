afb06645f27caa5e92f028d2853d5b8ad97694e8
D. Paolella <dpaolell@redhat.com>
2023-03-20 15:30:50 +0100

    Update plashets remote host to ocp-artifacts

--- pyartcd/pyartcd/constants.py
+++ pyartcd/pyartcd/constants.py
@@ -1,6 +1,6 @@
-PLASHET_REMOTE_URL = "http://download.eng.bos.redhat.com/rcm-guest/puddles/RHAOS/plashets"
-PLASHET_REMOTE_HOST = "ocp-build@rcm-guest.app.eng.bos.redhat.com"
-PLASHET_REMOTE_BASE_DIR = "/mnt/rcm-guest/puddles/RHAOS/plashets"
+PLASHET_REMOTE_URL = "https://ocp-artifacts.hosts.prod.psi.rdu2.redhat.com/pub/RHOCP/plashets"
+PLASHET_REMOTE_HOST = "ocp-artifacts"
+PLASHET_REMOTE_BASE_DIR = "/mnt/data/pub/RHOCP/plashets"
 
 TARBALL_SOURCES_REMOTE_HOST = "ocp-build@rcm-guest.app.eng.bos.redhat.com"
 TARBALL_SOURCES_REMOTE_BASE_DIR = "/mnt/rcm-guest/ocp-client-handoff"

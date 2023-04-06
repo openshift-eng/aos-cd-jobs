f5c5b5c4a638fb0aa951d65ee90dc8c04b77da4f
Ximin han <ximhan@redhat.com>
2023-04-06 16:10:02 +0800

    fix syntax error

--- pipeline-scripts/release.groovy
+++ pipeline-scripts/release.groovy
@@ -502,18 +502,18 @@ def stagePublishClient(quay_url, from_release_tag, release_name, arch, client_ty
     sh "mkdir -p ${CLIENT_MIRROR_DIR}"
 
     // Find the GitHub commit id for cli and download the repo at that commit, then publish it.
-    def download_cli_tarball = """
+    def download_cli_tarball = '''
         if oc adm release info ${quay_url}:${from_release_tag} --image-for=cli ; then
             commit=$(oc image info --output json `oc adm release info ${quay_url}:${from_release_tag} --image-for=cli` | jq -r '.config.config.Labels."io.openshift.build.commit.id"')
             curl -L -o "${CLIENT_MIRROR_DIR}/oc-source.tar.gz" https://github.com/openshift/oc/archive/${commit}.tar.gz
         fi
-    """
+    '''
     commonlib.shell(script: download_cli_tarball)
 
     if ( arch == 'x86_64' ) {
         // oc image  extract requires an empty destination directory. So do this before extracting tools.
         // oc adm release extract --tools does not require an empty directory.
-        def oc_mirror_extract_cmd = """
+        def oc_mirror_extract_cmd = '''
             # If the release payload contains an oc-mirror artifact image, then extract the oc-mirror binary.
             if oc adm release info ${quay_url}:${from_release_tag} --image-for=oc-mirror ; then
                 MOBY_DISABLE_PIGZ=true GOTRACEBACK=all oc image extract `oc adm release info ${quay_url}:${from_release_tag} --image-for=oc-mirror` --path /usr/bin/oc-mirror:${CLIENT_MIRROR_DIR}
@@ -523,7 +523,7 @@ def stagePublishClient(quay_url, from_release_tag, release_name, arch, client_ty
                 rm oc-mirror
                 popd
             fi
-        """
+        '''
         commonlib.shell(script: oc_mirror_extract_cmd)
     }
 

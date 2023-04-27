28bce2ef7ef7fed49b69b64e40833945ba6430ef
D. Paolella <dpaolell@redhat.com>
2023-04-27 09:46:03 +0200

    Move sync_repo_to_s3_mirror to dedicated file; delete old rpms by default

--- pyartcd/pyartcd/pipelines/ocp4.py
+++ pyartcd/pyartcd/pipelines/ocp4.py
@@ -1,45 +1,12 @@
-import os
 import traceback
 
 import click
 from aioredlock import LockError
 
-from pyartcd import exectools, locks
+from pyartcd import locks
 from pyartcd.cli import cli, pass_runtime, click_coroutine
 from pyartcd.runtime import Runtime
-
-
-async def sync_repo_to_s3_mirror(local_dir: str, s3_path: str, dry_run: bool):
-    if not s3_path.startswith('/') or \
-            s3_path.startswith('/pub/openshift-v4/clients') or \
-            s3_path.startswith('/pub/openshift-v4/amd64') or \
-            s3_path.startswith('/pub/openshift-v4/arm64') or \
-            s3_path.startswith('/pub/openshift-v4/dependencies'):
-        raise Exception(
-            f'Invalid location on s3 ({s3_path}); these are virtual/read-only locations on the s3 '
-            'backed mirror. Qualify your path with /pub/openshift-v4/<brew_arch_name>/ instead.')
-
-    # Sync is not transactional. If we update repomd.xml before files it references are populated,
-    # users of the repo will get a 404. So we run in three passes:
-    # 1. On the first pass, exclude files like repomd.xml and do not delete any old files.
-    #    This ensures that we  are only adding new rpms, filelist archives, etc.
-    # 2. On the second pass, include only the repomd.xml.
-    base_cmd = ['aws', 's3', 'sync', '--no-progress', '--exact-timestamps']
-    if dry_run:
-        base_cmd.append('--dryrun')
-
-    cmd = base_cmd + [
-        '--exclude', '*/repomd.xml', local_dir,
-        f's3://art-srv-enterprise{s3_path}'  # Note that s3_path has / prefix.
-    ]
-    env = os.environ.copy()
-    await exectools.cmd_assert_async(cmd, env=env)
-
-    cmd = base_cmd + [
-        '--exclude', '*', '--include', '*/repomd.xml', local_dir,
-        f's3://art-srv-enterprise{s3_path}'
-    ]
-    await exectools.cmd_assert_async(cmd, env=env)
+from pyartcd.s3 import sync_repo_to_s3_mirror
 
 
 @cli.command("ocp4:mirror-rpms",
---
+++ pyartcd/pyartcd/s3.py
@@ -0,0 +1,45 @@
+import os
+
+from pyartcd import exectools
+
+
+async def sync_repo_to_s3_mirror(local_dir: str, s3_path: str, dry_run: bool = False, remove_old: bool = True):
+    if not s3_path.startswith('/') or \
+            s3_path.startswith('/pub/openshift-v4/clients') or \
+            s3_path.startswith('/pub/openshift-v4/amd64') or \
+            s3_path.startswith('/pub/openshift-v4/arm64') or \
+            s3_path.startswith('/pub/openshift-v4/dependencies'):
+        raise Exception(
+            f'Invalid location on s3 ({s3_path}); these are virtual/read-only locations on the s3 '
+            'backed mirror. Qualify your path with /pub/openshift-v4/<brew_arch_name>/ instead.')
+
+    full_s3_path = f's3://art-srv-enterprise{s3_path}'  # Note that s3_path has / prefix.
+
+    # Sync is not transactional. If we update repomd.xml before files it references are populated,
+    # users of the repo will get a 404. So we run in three passes:
+    # 1. On the first pass, exclude files like repomd.xml and do not delete any old files.
+    #    This ensures that we  are only adding new rpms, filelist archives, etc.
+    # 2. On the second pass, include only the repomd.xml.
+    base_cmd = ['aws', 's3', 'sync', '--no-progress', '--exact-timestamps']
+    if dry_run:
+        base_cmd.append('--dryrun')
+
+    cmd = base_cmd + [
+        '--exclude', '*/repomd.xml', local_dir, full_s3_path
+    ]
+    env = os.environ.copy()
+    await exectools.cmd_assert_async(cmd, env=env)
+
+    cmd = base_cmd + [
+        '--exclude', '*', '--include', '*/repomd.xml', local_dir, full_s3_path
+    ]
+    await exectools.cmd_assert_async(cmd, env=env)
+
+    # For most repos, clean up the old rpms so they don't grow unbounded. Specify remove_old=false to prevent this step.
+    # Otherwise:
+    # 3. Everything should be synced in a consistent way -- delete anything old with --delete.
+    if remove_old:
+        cmd = base_cmd + [
+            '--delete', local_dir, full_s3_path
+        ]
+        await exectools.cmd_assert_async(cmd, env=env)

09fbb8b8a1fde448da7cc94f85a45416d5a43045
Luke Meyer <lmeyer@redhat.com>
2023-06-12 11:46:07 -0400

    [ART-7039] provide client cert to rhsm-pulp

--- pipeline-scripts/buildlib.groovy
+++ pipeline-scripts/buildlib.groovy
@@ -165,7 +165,17 @@ def setup_venv(use_python38=false) {
 }
 
 def doozer(cmd, opts=[:]){
-    withCredentials([usernamePassword(credentialsId: 'art-dash-db-login', passwordVariable: 'DOOZER_DB_PASSWORD', usernameVariable: 'DOOZER_DB_USER')]) {
+    withCredentials([
+        usernamePassword(
+            credentialsId: 'art-dash-db-login',
+            passwordVariable: 'DOOZER_DB_PASSWORD', usernameVariable: 'DOOZER_DB_USER'),
+        file(
+            credentialsId: 'art-jenkins-ldap-serviceaccount-private-key',
+            variable: 'RHSM_PULP_KEY')),
+        file(
+            credentialsId: 'art-jenkins-ldap-serviceaccount-client-cert',
+            variable: 'RHSM_PULP_CERT')),
+    ]) {
         withEnv(['DOOZER_DB_NAME=art_dash']) {
             return commonlib.shell(
                 returnStdout: opts.capture ?: false,

24acff631b1ad3d67bdd0e9bf6fe1d794396056a
D. Paolella <dpaolell@redhat.com>
2023-06-27 11:48:35 +0200

    Make tests async when needed

--- pyartcd/tests/pipelines/test_tarball_sources.py
+++ pyartcd/tests/pipelines/test_tarball_sources.py
@@ -1,15 +1,12 @@
-from unittest import TestCase
-from mock import Mock, AsyncMock, patch, ANY
-from asyncio import get_event_loop
-
-from mock.mock import MagicMock
+from unittest import IsolatedAsyncioTestCase
+from unittest.mock import Mock, AsyncMock, patch, ANY, MagicMock
 
 from pyartcd.pipelines.tarball_sources import TarballSourcesPipeline
 
 
-class TestTarballSourcesPipeline(TestCase):
+class TestTarballSourcesPipeline(IsolatedAsyncioTestCase):
     @patch("pyartcd.exectools.cmd_gather_async")
-    def test_create_tarball_sources(self, cmd_gather_async: Mock):
+    async def test_create_tarball_sources(self, cmd_gather_async: Mock):
         cmd_gather_async.return_value = (0, """
 Created tarball source /mnt/nfs/home/jenkins/yuxzhu/OSE-4.6-RHEL-8/84693/release/logging-fluentd-container-v4.6.0-202111191944.p0.gf73a1dd.assembly.stream.tar.gz.
 
@@ -26,14 +23,14 @@ OSE-4.6-RHEL-8/84693/release/logging-fluentd-container-v4.6.0-202111191944.p0.gf
         """, "")
         expected = ['OSE-4.6-RHEL-8/84693/release/logging-fluentd-container-v4.6.0-202111191944.p0.gf73a1dd.assembly.stream.tar.gz']
         pipeline = TarballSourcesPipeline(MagicMock(dry_run=False), "fake-group-4.10", "fake-assembly", ["fake-component"], [])
-        actual = get_event_loop().run_until_complete(pipeline._create_tarball_sources([10000, 10001], "fake-working/sources"))
+        actual = await pipeline._create_tarball_sources([10000, 10001], "fake-working/sources")
         self.assertEqual(actual, expected)
 
     @patch("pyartcd.exectools.cmd_assert_async")
-    def test_copy_to_rcm_guest(self, cmd_assert_async: AsyncMock):
+    async def test_copy_to_rcm_guest(self, cmd_assert_async: AsyncMock):
         cmd_assert_async.return_value = (0, "whatever", "whatever")
         pipeline = TarballSourcesPipeline(MagicMock(dry_run=False), "fake-group-4.10", "fake-assembly", ["fake-component"], [])
-        get_event_loop().run_until_complete(pipeline._copy_to_spmm_utils("fake-working/sources"))
+        await pipeline._copy_to_spmm_utils("fake-working/sources")
         cmd_assert_async.assert_awaited_once_with([
             'rsync', '-avz', '--no-perms', '--no-owner', '--omit-dir-times', '--no-group', 'fake-working/sources', ANY])
 
@@ -46,14 +43,14 @@ OSE-4.6-RHEL-8/84693/release/logging-fluentd-container-v4.6.0-202111191944.p0.gf
         self.assertEqual(actual, jira_client.create_issue.return_value)
 
     @patch("pyartcd.util.load_group_config")
-    def test_run(self, load_group_config: AsyncMock):
+    async def test_run(self, load_group_config: AsyncMock):
         runtime = MagicMock(dry_run=False)
         load_group_config.return_value = {"advisories": {"extras": 10000, "image": 10001}}
         pipeline = TarballSourcesPipeline(runtime, "fake-group-4.10", "fake-assembly", ["fake-component"], [])
         pipeline._create_tarball_sources = AsyncMock(return_value=["source-1.tar.gz", "source-2.tar.gz"])
         pipeline._copy_to_spmm_utils = AsyncMock()
         pipeline._create_jira = MagicMock(return_value=MagicMock(key="FAKE-123"))
-        get_event_loop().run_until_complete(pipeline.run())
+        await pipeline.run()
         load_group_config.assert_awaited_once_with("fake-group-4.10", "fake-assembly", ANY)
         pipeline._create_tarball_sources.assert_awaited_once_with([10000, 10001], ANY)
         pipeline._copy_to_spmm_utils.assert_awaited_once_with(ANY)

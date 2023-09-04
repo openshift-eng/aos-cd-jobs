b4fe47cc2835c5e03bf3c14667afc94953f54dbe
AOS Automation Release Team <noreply@redhat.com>
2023-09-04 17:13:09 +0000

    Auto-generated commit from 5f1b4f9

--- pyartcd/pyartcd/__main__.py
+++ pyartcd/pyartcd/__main__.py
@@ -4,7 +4,7 @@ from pyartcd.cli import cli
 from pyartcd.pipelines import (
     build_microshift, check_bugs, gen_assembly, prepare_release, promote, rebuild, report_rhcos,
     review_cvp, tarball_sources, build_sync, build_rhcos, ocp4_scan, images_health, operator_sdk_sync,
-    olm_bundle, ocp4, scan_for_kernel_bugs, tag_rpms, advisory_drop
+    olm_bundle, ocp4, scan_for_kernel_bugs, tag_rpms, advisory_drop, cleanup_locks
 )
 
 

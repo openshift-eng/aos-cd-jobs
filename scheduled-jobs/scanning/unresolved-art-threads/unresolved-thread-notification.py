778a365072c8bf3e3b92b51b8c6c2ff2bc6180ed
Siddharth Sharma <sidsharm@redhat.com>
2024-06-04 10:59:54 -0400

    format tweaks

--- scheduled-jobs/scanning/unresolved-art-threads/unresolved-thread-notification.py
+++ scheduled-jobs/scanning/unresolved-art-threads/unresolved-thread-notification.py
@@ -3,10 +3,11 @@
 import json
 import os
 import time
-import datetime
 import requests
 import sys
+from datetime import datetime, timezone
 from collections import OrderedDict
+from urllib.parse import unquote
 
 from slack_bolt import App
 from slack_sdk.errors import SlackApiError
@@ -17,22 +18,24 @@ USER_TOKEN = os.getenv('SLACK_USER_TOKEN')
 CHANNEL = os.getenv('CHANNEL')
 JENKINS_TOKEN = os.getenv('JENKINS_SERVICE_ACCOUNT_TOKEN')
 JENKINS_USER = os.getenv('JENKINS_SERVICE_ACCOUNT')
+JENKINS_URL = os.getenv('JENKINS_URL').rstrip('/')
 
 RELEASE_ARTIST_HANDLE = 'release-artists'
 
-def get_failed_jobs_json():
-    url = "https://art-jenkins.apps.prod-stable-spoke1-dc-iad2.itup.redhat.com/job/aos-cd-builds/api/json"
+def get_failed_jobs_text():
+    aos_cd_builds_url = f"{JENKINS_URL}/job/aos-cd-builds"
+    api_url = f"{aos_cd_builds_url}/api/json"
     query = "?tree=jobs[name,builds[number,result,timestamp]]"  # status,displayName are also useful
-    response = requests.get(url+query, auth=(JENKINS_USER, JENKINS_TOKEN))
+    response = requests.get(api_url+query, auth=(JENKINS_USER, JENKINS_TOKEN))
     response.raise_for_status()
     data = response.json()
     failed_jobs = []
-    now = datetime.datetime.now(datetime.UTC)
+    now = datetime.now(timezone.utc)
     for job in data['jobs']:
         job_name = job['name']
         f = 0
         for build in job['builds']:
-            dt = datetime.datetime.fromtimestamp(build['timestamp']/1000, datetime.UTC)
+            dt = datetime.fromtimestamp(build['timestamp']/1000, tz=timezone.utc)
             td = now - dt
             hours = td.seconds//3600
             minutes = (td.seconds//60)%60
@@ -43,16 +46,19 @@ def get_failed_jobs_json():
         if f > 0:
             failed_jobs.append((job_name, f))
 
+    def job_link(job_name):
+        link = f"{aos_cd_builds_url}/job/{job_name}/"
+        return f"<{link}|{unquote(job_name)}>"
+
     if failed_jobs:
         failed_jobs.sort(key=lambda x: x[1], reverse=True)
-        return json.dumps(failed_jobs)
-    return None
+        failed_jobs_list = "\n".join([f"* {job_link(job[0])}: {job[1]}" for job in failed_jobs])
+        failed_jobs_text = f"Failed aos-cd-jobs in last 3 hours: \n{failed_jobs_list}"
+        return failed_jobs_text
+    return ''
 
 def main():
-    failed_jobs_json = get_failed_jobs_json()
-    failed_jobs_text = ''
-    if failed_jobs_json:
-        failed_jobs_text = f"Failed aos-cd-jobs in last 3 hours:```{failed_jobs_json}```"
+    failed_jobs_text = get_failed_jobs_text()
 
     app = App(
         token=SLACK_API_TOKEN,
@@ -118,7 +124,7 @@ def main():
 
         fallback_text += f'\n{permalink}'
         timestamp = int(float(match.get('ts', '0.0')))
-        str_date = datetime.datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+        str_date = datetime.fromtimestamp(timestamp, tz=timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
 
         age = current_epoch_time - timestamp
         age_type = 'seconds'

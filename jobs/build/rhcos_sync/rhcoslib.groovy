58e91287768f78db1e35230cee46daa99be0c1f5
Siddharth Sharma <sidsharm@redhat.com>
2024-11-19 20:42:18 -0500

    tweaks

--- jobs/build/rhcos_sync/rhcoslib.groovy
+++ jobs/build/rhcos_sync/rhcoslib.groovy
@@ -3,7 +3,6 @@ buildlib = releaselib.buildlib
 commonlib = releaselib.commonlib
 rhcosWorking = "rhcos_working"
 logLevel = ""
-dryRun = ""
 artifacts = []
 baseUrl = ""
 metaUrl = ""
@@ -17,41 +16,15 @@ rhcos_allowlist = [ "gcp", "initramfs", "iso", "kernel", "metal", "openstack", "
 def initialize(ocpVersion, rhcosBuild, arch, name, mirrorPrefix) {
     buildlib.cleanWorkdir(rhcosWorking)
 
-    // Example URL paths (visit https://releases-rhcos-art.apps.ocp-virt.prod.psi.redhat.com/ to view yourself):
-    // 4.1 with x86 arch:       releases/rhcos-4.1/410.81.20200213.0/meta.json  (we do not plan to release a new 4.1 bootimage ever)
-    // 4.1 with non-x86 arch:   N/A
+    baseUrl = buildlib.doozer("--quiet --group=openshift-${ocpVersion} config:read-group urls.rhcos_release_base.multi --default ''", [capture: true]).trim()
+    baseUrl = "${baseUrl}/${rhcosBuild}/${arch}"
 
-    // 4.2 introduced an arch arch suffix
-    // 4.2 with x86 arch:       releases/rhcos-4.2/42.81.20200213.0/meta.json
-    // 4.2 with non-x86 arch:   releases/rhcos-4.2-s390x/42s390x.81.20200131.0/meta.json
-
-    // 4.3 introduced an arch subdirectory
-    // 4.3 with x86 arch:       releases/rhcos-4.3/43.81.202002130953.0/x86_64/meta.json
-    // 4.3 with non-x86 arch:   releases/rhcos-4.3-s390x/43.81.202001300441.0/s390x/meta.json
-
-    // Sub in some vars according to params
-    def archSuffix = commonlib.brewSuffixForArch(arch)
-    def archDir = ocpVersion == "4.2" ? "" : "/${arch}"
-    baseUrl = "https://art-rhcos-ci.s3.amazonaws.com/releases/rhcos-${ocpVersion}${archSuffix}/${rhcosBuild}${archDir}"
-    // Since 4.9+, rhcos meta is placed at a different location.
-    def (major, minor) = commonlib.extractMajorMinorVersionNumbers(ocpVersion)
-    if (major > 4 || (major == 4 && minor >=9)) {
-        baseUrl = "https://releases-rhcos-art.apps.ocp-virt.prod.psi.redhat.com/storage/prod/streams/$ocpVersion/builds/$rhcosBuild/$arch"
-        if (minor >= 16) {
-            baseUrl = "https://releases-rhcos-art.apps.ocp-virt.prod.psi.redhat.com/storage/prod/streams/$ocpVersion-9.4/builds/$rhcosBuild/$arch"
-        } else if (minor >= 13) {
-            baseUrl = "https://releases-rhcos-art.apps.ocp-virt.prod.psi.redhat.com/storage/prod/streams/$ocpVersion-9.2/builds/$rhcosBuild/$arch"
-        }
-    }
     s3MirrorBaseDir = "/pub/openshift-v4/${arch}/dependencies/rhcos"
-    // Actual meta.json
-    metaUrl = baseUrl + "/meta.json"
+    metaUrl = "${baseUrl}/meta.json"
 
     currentBuild.displayName = "${name} - ${rhcosBuild}:${arch} - ${mirrorPrefix}"
     currentBuild.description = "Meta JSON: ${metaUrl}"
-
     if ( params.DRY_RUN ) {
-        dryRun = "--dry-run=true"
         currentBuild.displayName += " [DRY_RUN]"
     }
 
@@ -87,7 +60,7 @@ def getRhcosBuildFromMirror(rhcosMirrorPrefix, name) {
     return rhcosId
 }
 
-def rhcosSyncMirrorArtifacts(rhcosMirrorPrefix, arch, rhcosBuild, name) {
+def rhcosSyncMirrorArtifacts(rhcosMirrorPrefix, arch, rhcosBuild, name, noLatest) {
     // check if rhcos-id is already on the mirror
     def rhcosBuildOnMirror = getRhcosBuildFromMirror(rhcosMirrorPrefix, name)
     echo("RHCOS build requested to sync: ${rhcosBuild}")
@@ -111,7 +84,7 @@ def rhcosSyncMirrorArtifacts(rhcosMirrorPrefix, arch, rhcosBuild, name) {
     if ( params.DRY_RUN ) {
             invokeOpts += " --test"
     }
-    if ( params.NO_LATEST ) {
+    if ( noLatest ) {
             invokeOpts += " --nolatest"
     }
 

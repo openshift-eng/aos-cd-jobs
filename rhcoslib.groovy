816646f2cba338737073c2445250ad5cd19d312b
AOS Automation Release Team <noreply@redhat.com>
2026-06-18 14:21:02 +0000

    Auto-generated commit from 55461b7

--- rhcoslib.groovy
+++ rhcoslib.groovy
@@ -25,10 +25,8 @@ def initialize(ocpVersion, rhcosBuild, arch, name, mirrorPrefix) {
     buildParts = rhcosBuild.split("\\.")
     ocpStream = ocpVersion
     minor = ocpVersion.split("\\.")
-    majorNum = minor[0].asType(Integer)
-    minorNum = minor[1].asType(Integer)
-    if ( majorNum > 4 || (majorNum == 4 && minorNum > 18) ) {
-        // For 4.19+ / 5.x+ the rhcosBuild name is in format 9.6.20250121-0
+    if ( minor[1].toInteger() > 18 ) {
+        // For 4.19 the rhcosBuild name is in format 9.6.20250121-0
         rhelStream = buildParts[0] + '.' + buildParts[1]
         stream = "rhel-${rhelStream}"
     } else {
